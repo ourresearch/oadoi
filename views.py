bfefe344f15e61e9eef81143a01d35c2ff51172c
Heather Piwowar <hpiwowar@gmail.com>
2015-03-27 10:13:03 -0700

    move views in to a views.py file

---
+++ views.py
@@ -0,0 +1,11 @@
+from app import app
+import os
+from flask import Flask
+
+@app.route("/")
+def hello():
+    return "Hello world!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5008))
+    app.run(host='0.0.0.0', port=port)
