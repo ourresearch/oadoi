4ff6d50a1c06fa7114d08d3ea372c2224a88778f
Jason Priem <jp@jasonpriem.org>
2015-03-29 18:29:45 -0700

    less compiling to css and being applied to page.

--- static/src/app.js
+++ static/src/app.js
@@ -1,1 +1,70 @@
-alert("is grunt watch working?")
+angular.module('app', [
+  'ngRoute', // loaded from external lib
+  'templates.app'  // this is how it accesses the cached templates in ti.js
+]);
+
+
+
+angular.module('app').config(function ($routeProvider,
+                                       $locationProvider) {
+  $locationProvider.html5Mode(true);
+//  paginationTemplateProvider.setPath('directives/pagination.tpl.html')
+
+  // want to make sure the user profile route loads last, because it's super greedy.
+  $routeProvider.when("/", {
+    templateUrl:'landing.tpl.html'
+  })
+});
+
+
+angular.module('app').run(function($route,
+                                   $rootScope,
+                                   $timeout,
+                                   $location) {
+
+  /*
+  this lets you change the args of the URL without reloading the whole view. from
+     - https://github.com/angular/angular.js/issues/1699#issuecomment-59283973
+     - http://joelsaupe.com/programming/angularjs-change-path-without-reloading/
+     - https://github.com/angular/angular.js/issues/1699#issuecomment-60532290
+  */
+  var original = $location.path;
+  $location.path = function (path, reload) {
+      if (reload === false) {
+          var lastRoute = $route.current;
+          var un = $rootScope.$on('$locationChangeSuccess', function () {
+              $route.current = lastRoute;
+              un();
+          });
+        $timeout(un, 500)
+      }
+      return original.apply($location, [path]);
+  };
+
+});
+
+
+angular.module('app').controller('AppCtrl', function($scope){
+
+  console.log("we loaded the app controller (AppCtrl)")
+  /*
+  $scope.$on('$routeChangeError', function(event, current, previous, rejection){
+    RouteChangeErrorHandler.handle(event, current, previous, rejection)
+  });
+
+  $scope.$on('$routeChangeSuccess', function(next, current){
+    security.requestCurrentUser().then(function(currentUser){
+      Page.sendPageloadToSegmentio()
+    })
+  })
+
+  $scope.$on('$locationChangeStart', function(event, next, current){
+    ProductPage.loadingBar(next, current)
+    Page.setProfileUrl(false)
+    Loading.clear()
+  })
+
+  */
+
+});
+
