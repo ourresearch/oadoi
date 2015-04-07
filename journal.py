c62a1e075a0c73b63414432e3768f1881333779d
Heather Piwowar <hpiwowar@gmail.com>
2015-04-07 11:30:46 -0700

    add cleaned up journals list

--- journal.py
+++ journal.py
@@ -1,7 +1,7 @@
 import os
 import re
 import codecs
-from journals_list import journals_list
+from journals_list import pubmed_journals
 
 def create_journals_list_from_medline_dump():
     # get a medline dump like this:
@@ -10,10 +10,6 @@ def create_journals_list_from_medline_dump():
     # Send To as Summary (text)    
     # put it in data/pubmed_journals.txt then run this
 
-    global journals_list
-
-    journals_list = journals_list
-
     current_dir = os.path.dirname(__file__)
     rel_path_to_unis_csv = "data/pubmed_journals.txt"
     absolute_path_to_unis_csv = os.path.join(current_dir, rel_path_to_unis_csv)
@@ -21,15 +17,28 @@ def create_journals_list_from_medline_dump():
     with codecs.open(absolute_path_to_unis_csv, "r", "utf-8") as myfile:
         journals_str = myfile.read()
         journal_pattern = re.compile("\d+\. (.+?)\nISSN:", re.DOTALL)
-        journals_list = journal_pattern.findall(journals_str)
+        journals_list_rough = journal_pattern.findall(journals_str)
 
-    journals_list = [j.lower() for j in journals_list]
+    journals_list_clean = {}
+    for orig_journal in journals_list_rough:
+        clean_journal = orig_journal
+        clean_journal = clean_journal.replace("\n", " ")
+        clean_journal = clean_journal.replace("&amp", "&")
+        clean_journal = clean_journal.replace("&;", "&")
+        clean_journal = clean_journal.split("(")[0]
+        clean_journal = clean_journal.split("/")[0]
+        clean_journal = clean_journal.split("=")[0]
+        clean_journal = clean_journal.split(" : ")[0]
+        clean_journal = clean_journal.strip()
+        journals_list_clean[clean_journal] = orig_journal
+
+    return journals_list_clean
 
 
 
 def filter_journal_list(name_starts_with, max_len=8):
     lower_name = name_starts_with.lower()
-    results = [journal for journal in journals_list if journal.startswith(lower_name)]
+    results = [journal for journal in pubmed_journals if journal.lower().startswith(lower_name)]
     return results[0:max_len]
 
 
