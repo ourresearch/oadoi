75ccc5c0f3ccc11e383c2dc90503d40e3f3bc58f
Jason Priem <jp@jasonpriem.org>
2016-02-21 19:04:32 -0800

    site toolbar stays same height.

--- static/dist/ti.js
+++ static/dist/ti.js
@@ -1851,8 +1851,10 @@ angular.module("static-pages/about.tpl.html", []).run(["$templateCache", functio
 angular.module("static-pages/landing.tpl.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("static-pages/landing.tpl.html",
     "<!-- the landing page for people who are not logged in -->\n" +
-    "<div class=\"landing static-page\" ng-show=\"!auth.isAuthenticated()\">\n" +
-    "    <div class=\"tagline\" layout=\"column\" layout-align=\"center center\">\n" +
+    "<div class=\"landing static-page\"\n" +
+    "     layout=\"column\" layout-align=\"center center\"\n" +
+    "     ng-show=\"!auth.isAuthenticated()\">\n" +
+    "    <div class=\"tagline\">\n" +
     "        <h1>\n" +
     "            Find the online impact of your research\n" +
     "        </h1>\n" +
