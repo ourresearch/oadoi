9a32a4347aec882bb58e255af063d32970bc0ef7
Jason Priem <jp@jasonpriem.org>
2016-03-10 18:00:30 -0800

    person service with old profileService code as stub.

--- static/src/services/person.js
+++ static/src/services/person.js
@@ -1,3 +1,54 @@
-/**
- * Created by jay on 3/10/16.
- */
+angular.module('person', [
+  ])
+
+
+
+  .factory("Person", function($http,
+                              $timeout,
+                              $location){
+
+    var data = {}
+
+    function profileStillLoading(){
+      console.log("testing if profile still loading", data.profile.articles)
+      return _.any(data.profile.articles, function(article){
+        return _.isNull(article.percentile)
+      })
+    }
+
+    function getProfile(slug){
+      var url = "/profile/" + slug
+      console.log("getting profile for", slug)
+      return $http.get(url).success(function(resp){
+        data.profile = resp
+
+        if (profileStillLoading()){
+          $timeout(function(){
+            getProfile(slug)
+          }, 1000)
+        }
+
+      })
+    }
+
+    return {
+      data: data,
+      createProfile: function(name, pmids, coreJournals) {
+        console.log("i am making a profile:", name, pmids)
+        var postData = {
+          name: name,
+          pmids: pmids,
+          core_journals: coreJournals
+        }
+        $http.post("/profile",postData)
+          .success(function(resp, status, headers){
+            console.log("yay got a resp from /profile!", resp)
+            $location.path("/u/" + resp.slug)
+          })
+      },
+
+      getProfile: getProfile
+    }
+
+
+  })
