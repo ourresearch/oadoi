aba2cc8f1ec9d95bad158258d30105a0a5f4e79b
Jason Priem <jp@jasonpriem.org>
2016-05-15 19:01:53 -0700

    adds mendeley icon.

--- models/source.py
+++ models/source.py
@@ -83,10 +83,10 @@ class Source(object):
             if my_product.post_counts:
                 if self.source_name in my_product.post_counts:
                     post_counts += int(my_product.post_counts[self.source_name])
-                # elif self.source_name == "mendeley":
-                #     if my_product.mendeley_api_raw and "reader_count" in my_product.mendeley_api_raw:
-                #         mendeley_readers = my_product.mendeley_api_raw["reader_count"]
-                #         post_counts += mendeley_readers
+                elif self.source_name == "mendeley":
+                    if my_product.mendeley_api_raw and "reader_count" in my_product.mendeley_api_raw:
+                        mendeley_readers = my_product.mendeley_api_raw["reader_count"]
+                        post_counts += mendeley_readers
         return post_counts
 
     @property
