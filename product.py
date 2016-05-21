3efa3b5d859a0bbd48022d9a6df70a9f2d76cd34
Jason Priem <jp@jasonpriem.org>
2016-05-20 23:34:45 -0700

    tests actually work.

--- product.py
+++ product.py
@@ -6,6 +6,7 @@ from time import time
 import inspect
 import sys
 import re
+from threading import Thread
 
 
 def get_oa_url(url):
@@ -29,36 +30,45 @@ def get_oa_url(url):
 class Tests(object):
     def __init__(self):
         self.passed = []
-        self.failed = []
-        self.count = 0
         self.elapsed = 0
 
 
     def run(self):
         start = time()
+        
+        # get all the test pairs
         this_module = sys.modules[__name__]
         file_source = inspect.getsource(this_module)
-
         p = re.compile(ur'^\s+=(.+)\n\s+=(.+)', re.MULTILINE)
         test_pairs = re.findall(p, file_source)
+        
+        # start a thread for each test pair,
+        # and save the results in a single shared list, test_results
+        threads = []
+        test_results = []
+        for url, expected_output in test_pairs:
+            process = Thread(target=test_url_for_threading, args=[url, expected_output, test_results])
+            process.start()
+            threads.append(process)
+    
+        # wait till all work is done
+        for process in threads:
+            process.join()
 
-        for url, expected_result in test_pairs:
-            test_result = test_url(url, expected_result)
-
-            if test_result["passed"]:
-                self.passed.append(test_result)
-            else:
-                self.failed.append(test_result)
-
-        self.count = len(self.passed) + len(self.failed)
+        # store the test results
+        self.results = test_results
         self.elapsed = elapsed(start)
 
 
+def test_url_for_threading(url, expected_output, all_test_results):
+    res = test_url(url, expected_output)
+    all_test_results.append(res)
+    return all_test_results
 
-def test_url(url, expected_result):
+def test_url(url, expected_output):
 
-    if expected_result == "None":
-        expected_result = None
+    if expected_output == "None":
+        expected_output = None
 
     my_start = time()
     result = get_oa_url(url)
@@ -67,7 +77,7 @@ def test_url(url, expected_result):
         "elapsed": elapsed(my_start),
         "url": url,
         "result": result,
-        "passed": result == expected_result
+        "passed": result == expected_output
     }
 
 
