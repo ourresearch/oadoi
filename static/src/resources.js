9b2596431158e2e67a81b1076d87e860be7d1bcb
Jason Priem <jp@jasonpriem.org>
2016-05-20 21:28:45 -0700

    giving up on grunt, will edit CSS bare.

--- static/src/resources.js
+++
@@ -1,9 +0,0 @@
-angular.module('resourcesModule', [])
-  .factory('Leaders', function($resource) {
-    return $resource('api/leaderboard')
-  })
-
-
-  .factory('PackageResource', function($resource) {
-    return $resource('/api/package/:namespace/:name')
-  })
