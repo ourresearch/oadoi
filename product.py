5249bccd352c124e22adeab74ae8860e9bad8674
Jason Priem <jp@jasonpriem.org>
2016-05-20 23:07:17 -0700

    testing page acutally shows stuff.

--- product.py
+++ product.py
@@ -1,30 +1,75 @@
 from util import elapsed
+
+import requests
+
 from time import time
-
-
+import inspect
+import sys
+import re
 
 
 def get_oa_url(url):
+    r = requests.get(url)
+    page = r.text
 
     """
     first test, on SelectedWorks site for ethan:
 
-    https://works.bepress.com/ethan_white/45/
-    https://works.bepress.com/ethan_white/45/download/
+    =https://works.bepress.com/ethan_white/45/
+    =https://works.bepress.com/ethan_white/45/download/
 
-    https://works.bepress.com/ethan_white/27/
-    None
+    =https://works.bepress.com/ethan_white/27/
+    =None
     """
+    return None
+
 
 
 
 class Tests(object):
+    def __init__(self):
+        self.passed = []
+        self.failed = []
+        self.count = 0
+        self.elapsed = 0
+
+
     def run(self):
         start = time()
+        this_module = sys.modules[__name__]
+        file_source = inspect.getsource(this_module)
 
-        self.passed = [1,2,3]
-        self.failed = [4,5,6]
+        p = re.compile(ur'^\s+=(.+)\n\s+=(.+)', re.MULTILINE)
+        test_pairs = re.findall(p, file_source)
+
+        for url, expected_result in test_pairs:
+            test_result = test_url(url, expected_result)
+
+            if test_result["passed"]:
+                self.passed.append(test_result)
+            else:
+                self.failed.append(test_result)
+
         self.count = len(self.passed) + len(self.failed)
         self.elapsed = elapsed(start)
+
+
+
+def test_url(url, expected_result):
+
+    if expected_result == "None":
+        expected_result = None
+
+    my_start = time()
+    result = get_oa_url(url)
+
+    return {
+        "elapsed": elapsed(my_start),
+        "url": url,
+        "result": result,
+        "passed": result == expected_result
+    }
+
+
 
 
--- views.py
+++ views.py
@@ -12,6 +12,7 @@ from flask import render_template
 import json
 import os
 import logging
+import sys
 logger = logging.getLogger("views")
 
 
@@ -59,6 +60,10 @@ def add_crossdomain_header(resp):
     resp.headers['Access-Control-Allow-Origin'] = "*"
     resp.headers['Access-Control-Allow-Methods'] = "POST, GET, OPTIONS, PUT, DELETE, PATCH"
     resp.headers['Access-Control-Allow-Headers'] = "origin, content-type, accept, x-requested-with"
+
+    # without this jason's heroku local buffers forever
+    sys.stdout.flush()
+
     return resp
 
 
@@ -71,6 +76,10 @@ def redirect_www_to_naked_domain():
             "http://sherlockoa.org"
         )
         return redirect(new_url, 301)  # permanent
+
+
+
+
 
 
 
