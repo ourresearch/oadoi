0a499db0e02e2dd2a9bb8bb95d4d2d8e7d8c280b
Jason Priem <jp@jasonpriem.org>
2016-05-21 22:03:47 -0700

    adding lots of new cases.

--- product.py
+++ product.py
@@ -24,12 +24,68 @@ def get_oa_url(url):
     """
     if they're telling us there is no fulltext, let's quit there.
 
-    =http://discovery.ucl.ac.uk/1450592/
+    @todo comment this back in when it stops being massively slow.
+    http://discovery.ucl.ac.uk/1450592/
+    None
+
+    =http://eprints.gla.ac.uk/20877/
+    =None
+
+    This is the (actions required) one. not sure. might generate false negatives
+    =http://nora.nerc.ac.uk/8783/
     =None
     """
-    if "full text not available" in page_words:
-        print "found 'full text not available' for ", url
-        return None
+    blacklist_phrases = [
+
+        "full text not available",
+        "full text not currently available",
+        "no files associated with this item",
+        "full-text and supplementary files are not available",
+
+        # not sure about this one
+        "(login required)"
+        ""
+
+    ]
+
+    """
+    if it's talking about how only admins can see it, it's not really open.
+
+    =http://sro.sussex.ac.uk/54348/
+    =None
+
+    =http://researchbank.acu.edu.au/fea_pub/434/
+    =None
+    """
+    blacklist_phrases += [
+        "admin only"
+    ]
+
+
+    """
+    let's find us some paywalls
+
+    =http://www.cell.com/trends/genetics/abstract/S0168-9525(07)00023-6
+    =None
+    """
+    blacklist_phrases.append("purchase access")
+
+
+    for phrase in blacklist_phrases:
+        if phrase in page_words:
+            print u"found a blacklist phrase: ", phrase, url
+            return None
+
+
+
+
+
+    # admin_only_matches = re.findall(ur"download(.*?)admin only", page_words)
+    # admin_only_matches += re.findall(ur"admin only(.*?)download", page_words)
+    # if admin_only_matches:
+    #     for m in admin_only_matches:
+    #         if len(m.split()) <= 3:
+    #             return None
 
 
 
@@ -67,21 +123,34 @@ def get_oa_url(url):
 
 
         """
-        download text is "download (54MB)":
+        download text has the word "download" it is somewhere, and the link is pointing to a PDF file:
 
         =http://eprints.whiterose.ac.uk/77866/
         =http://eprints.whiterose.ac.uk/77866/25/ggge20346_with_coversheet.pdf
+
+        note that researchgate can return various different things after the ? part of url.
+        makes for fussy testing but shouldn't matter much in production
+        =https://www.researchgate.net/publication/235915359_Promotion_of_Virtual_Research_Communities_in_CHAIN
+        =https://www.researchgate.net/profile/Bruce_Becker4/publication/235915359_Promotion_of_Virtual_Research_Communities_in_CHAIN/links/0912f5141bd165b4ef000000.pdf?origin=publication_detail
         """
         if "download" in link_text:
-            if link_target.endswith(".pdf"):
+            if len(re.findall(ur"\.pdf\b", link_target)):
                 return link_target
 
 
+
         """
-        download link says something.pdf and links to a .pdf
+        download link anchor text is something like foobar.pdf
 
         =http://hdl.handle.net/1893/372
         =http://dspace.stir.ac.uk/bitstream/1893/372/1/Corley%20COGNITION%202007.pdf
+
+        =https://research-repository.st-andrews.ac.uk/handle/10023/7421
+        =https://research-repository.st-andrews.ac.uk/bitstream/handle/10023/7421/Manuscripts_edited_final.pdf?sequence=1&isAllowed=y
+
+        probably superfluous:
+        =https://hal.inria.fr/hal-00839984
+        =https://hal.inria.fr/hal-00839984/document
         """
         if re.search(ur".\.pdf\b", link_text):
             return link_target
@@ -91,6 +160,9 @@ def get_oa_url(url):
 
 
     return None
+
+
+
 
 
 def get_abs_url(base_url, input_url):
