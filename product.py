7148d68b08979672d21f392f3f5e43b7a52e7c49
Jason Priem <jp@jasonpriem.org>
2016-05-23 19:44:56 -0700

    redo tests to return boolean for is_open, also take host as arg.

--- product.py
+++ product.py
@@ -13,28 +13,32 @@ import urlparse
 
 
 
-def get_oa_url(url, verbose=False):
+def is_oa(url, host, verbose=False):
     if verbose:
         print "getting URL: ", url
 
     with closing(requests.get(url, stream=True, timeout=100)) as r:
         # if our url redirects to a pdf, we're done.
-        # =http://hdl.handle.net/2060/20140010374
-        # =http://ntrs.nasa.gov/archive/nasa/casi.ntrs.nasa.gov/20140010374.pdf
+        # = open repo http://hdl.handle.net/2060/20140010374
         if head_says_pdf(r):
             print "the head says this is a PDF. we're quitting.", url
-            return r.url
+            return True
 
         # get the HTML tree and the bucket of words
         page = r.text
-        tree = html.fromstring(page)
         page = page.replace("&nbsp;", " ")  # otherwise starts-with for lxml doesn't work
+        tree = html.fromstring(page)
         page_words = " ".join(tree.xpath("//body")[0].text_content().lower().split())
 
 
         # tests that use the bucket of words
         if page_says_closed(page_words):
-            return None
+            return False
+
+        # if the page says open access on it, we just say that's the OA URL because it saves tons of time.
+        # open repo http://doi.org/10.1039/c5sm02502h
+        # if "open access" in page_words:
+        #     return r.url
 
 
         # tests that use the HTML tree
@@ -42,14 +46,18 @@ def get_oa_url(url, verbose=False):
         # if they are linking to a .docx or similar, this is probably open.
         doc_link = find_doc_download_link(tree, verbose)
         if doc_link is not None:
-            return urlparse.urljoin(r.url, sanitize_url(doc_link.attrib["href"]))
+            if verbose :
+                print "found OA link target (non-pdf): ", get_link_target(doc_link, r.url)
+            return True
 
         # if they are linking to a PDF, we need to follow the link to make sure it's legit
         pdf_download_link = find_pdf_link(tree, verbose)
         if pdf_download_link is not None:
-            return urlparse.urljoin(r.url, sanitize_url(pdf_download_link.attrib["href"]))
+            if verbose:
+                print "found OA link target: ", get_link_target(pdf_download_link, r.url)
+            return True
 
-        return None
+        return False
 
 
 
@@ -75,8 +83,7 @@ def find_doc_download_link(tree, verbose=False):
 
         # everything below is unique to this function.
 
-        # =https://lirias.kuleuven.be/handle/123456789/372010
-        # =https://lirias.kuleuven.be/bitstream/123456789/372010/3/NG-LE32489R3_manuscript.docx
+        # = open repo https://lirias.kuleuven.be/handle/123456789/372010
         if ".docx" in link_target:
             return link
 
@@ -122,14 +129,11 @@ def find_pdf_link(tree, verbose=False):
         """
         The download link doesn't have PDF at the end, but the download button is nice and clear.
 
-        =https://works.bepress.com/ethan_white/45/
-        =https://works.bepress.com/ethan_white/45/download/
+        = open repo https://works.bepress.com/ethan_white/45/
 
-        =https://works.bepress.com/ethan_white/27/
-        =None
+        = closed repo https://works.bepress.com/ethan_white/27/
 
-        =http://ro.uow.edu.au/aiimpapers/269/
-        =http://ro.uow.edu.au/cgi/viewcontent.cgi?article=1268&context=aiimpapers
+        = open repo http://ro.uow.edu.au/aiimpapers/269/
         """
         if link_text == "download":
             return link
@@ -139,13 +143,11 @@ def find_pdf_link(tree, verbose=False):
         download text has the word "download" it is somewhere. This is in a seperate
         block right now because we may need more precision on it (eg check the link_target for .pfd)
 
-        =http://eprints.whiterose.ac.uk/77866/
-        =http://eprints.whiterose.ac.uk/77866/25/ggge20346_with_coversheet.pdf
+        = open repo http://eprints.whiterose.ac.uk/77866/
 
         note that researchgate can return various different things after the ? part of url.
         makes for fussy testing but shouldn't matter much in production
-        =https://www.researchgate.net/publication/235915359_Promotion_of_Virtual_Research_Communities_in_CHAIN
-        =https://www.researchgate.net/profile/Bruce_Becker4/publication/235915359_Promotion_of_Virtual_Research_Communities_in_CHAIN/links/0912f5141bd165b4ef000000.pdf?origin=publication_detail
+        = open repo https://www.researchgate.net/publication/235915359_Promotion_of_Virtual_Research_Communities_in_CHAIN
         """
         if "download" in link_text:
             return link
@@ -155,11 +157,9 @@ def find_pdf_link(tree, verbose=False):
         """
         download link anchor text is something like foobar.pdf
 
-        =http://hdl.handle.net/1893/372
-        =http://dspace.stir.ac.uk/bitstream/1893/372/1/Corley%20COGNITION%202007.pdf
+        = open repo http://hdl.handle.net/1893/372
 
-        =https://research-repository.st-andrews.ac.uk/handle/10023/7421
-        =https://research-repository.st-andrews.ac.uk/bitstream/handle/10023/7421/Manuscripts_edited_final.pdf?sequence=1&isAllowed=y
+        = open repo https://research-repository.st-andrews.ac.uk/handle/10023/7421
         """
         if len(re.findall(ur".\.pdf\b", link_text)):
             return link
@@ -168,8 +168,7 @@ def find_pdf_link(tree, verbose=False):
         """
         the link anchor text is just "PDF"
 
-        =http://dro.dur.ac.uk/1241/
-        =http://dro.dur.ac.uk/1241/1/1241.pdf?DDD14+dgg1mbk+dgg0cnm
+        = open repo http://dro.dur.ac.uk/1241/
         """
         if link_text == "pdf":
             return link
@@ -179,8 +178,7 @@ def find_pdf_link(tree, verbose=False):
         """
         download link is identified with an image
 
-        =http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.587.8827
-        =http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.587.8827&rep=rep1&type=pdf
+        = open repo http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.587.8827
         """
         for img in link.findall("img"):
             try:
@@ -205,12 +203,10 @@ def page_says_closed(page_words):
     # "not in this repo" words
     blacklist_phrases = [
 
-        # =https://lirias.kuleuven.be/handle/123456789/9821
-        # =None
+        # = closed repo https://lirias.kuleuven.be/handle/123456789/9821
         "request a copy",
 
-        # =http://eprints.gla.ac.uk/20877/
-        # =None
+        # = closed repo http://eprints.gla.ac.uk/20877/
 
         "file restricted",
         "full text not available",
@@ -224,21 +220,17 @@ def page_says_closed(page_words):
         "does not have full-text",
 
         # not sure if we should keep this one, danger of false negs
-        # =http://nora.nerc.ac.uk/8783/
-        # =None
+        # = closed repo http://nora.nerc.ac.uk/8783/
         "(login required)",
 
-        # =http://sro.sussex.ac.uk/54348/
-        # =None
-        # =http://researchbank.acu.edu.au/fea_pub/434/
-        # =None
+        # = closed repo http://sro.sussex.ac.uk/54348/
+        # = closed repo http://researchbank.acu.edu.au/fea_pub/434/
         "admin only"
     ]
 
     # paywall words
     blacklist_phrases += [
-        # =http://www.cell.com/trends/genetics/abstract/S0168-9525(07)00023-6
-        # =None
+        # = closed repo http://www.cell.com/trends/genetics/abstract/S0168-9525(07)00023-6
         "purchase access"
     ]
 
@@ -252,8 +244,11 @@ def page_says_closed(page_words):
 def is_pdf_url(url):
     return len(re.findall(ur"\.pdf\b", url)) > 0
 
-def sanitize_url(url):
+def get_link_target(url, base_url):
     url = re.sub(ur";jsessionid=\w+", "", url)
+    if base_url:
+        url = urlparse.urljoin(base_url, url)
+
     return url
 
 
@@ -284,21 +279,18 @@ class Tests(object):
 
 
 class TestCase(object):
-    def __init__(self, url, expected_output, verbose=False):
+    def __init__(self, oa_expected=False, host="repo", url=None, verbose=False):
+        self.expected = oa_expected
+        self.host = host
         self.url = url
         self.verbose = verbose
-
-        if expected_output == "None":
-            self.expected = None
-        else:
-            self.expected = expected_output
 
         self.elapsed = None
         self.result = None
 
     def run(self):
         my_start = time()
-        self.result = get_oa_url(self.url, self.verbose)
+        self.result = is_oa(self.url, self.host, self.verbose)
         self.elapsed = elapsed(my_start)
 
     @property
@@ -313,25 +305,34 @@ def get_test_cases():
     # get all the test pairs
     this_module = sys.modules[__name__]
     file_source = inspect.getsource(this_module)
-    p = re.compile(ur'^[\s#]*=(.+)\n[\s#]*=(.+)', re.MULTILINE)
-    test_pairs = re.findall(p, file_source)
+    p = re.compile(ur'^[\s#]*=(.+)', re.MULTILINE)
+    test_lines = re.findall(p, file_source)
 
-    # first, see if we're supposed to only do one test
-    for url, expected_output in test_pairs:
-        if url.startswith("only: "):
-            url = url.replace("only: ", "")
-            single_case = TestCase(url, expected_output, verbose=True)
-            return [single_case]
+    for line in test_lines:
+        my_test_case = TestCase()
+        arg_list = line.split()
 
-    # now make the list of tests
-    for url, expected_output in test_pairs:
-        verbose = False
-        if url.startswith("verbose: "):
-            url.replace("verbose: ", "")
-            verbose = True
-        ret.append(
-            TestCase(url, expected_output, verbose=verbose)
-        )
+        # get the required URL
+        my_test_case.url = [arg for arg in arg_list if arg.startswith("http")][0]
+
+        # get optional things (optional because there are defaults set already)
+        if "open" in arg_list:
+            my_test_case.expected = True
+
+        if "journal" in arg_list:
+            my_test_case.host = "journal"
+
+        if "verbose" in arg_list:
+            my_test_case.verbose = True
+
+        # immediately quit and return this one if the "only" flag is set
+        if "only" in arg_list:
+            my_test_case.verbose = True
+            return [my_test_case]
+
+        # otherwise put this in the list and keep iterating
+        else:
+            ret.append(my_test_case)
 
     return ret
 
