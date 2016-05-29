143f17bc7df8bc1e0c06ecf69298915a80b9c8a5
Jason Priem <jp@jasonpriem.org>
2016-05-28 18:59:24 -0700

    no more bucket of words...only looks for download link.

--- product.py
+++ product.py
@@ -13,133 +13,68 @@ import urlparse
 
 
 
-def is_oa(url, host, verbose=False):
-    if verbose:
-        print "getting URL: ", url
+def is_oa(url, host):
+    print "getting URL: ", url
 
     with closing(requests.get(url, stream=True, timeout=100)) as r:
         # if our url redirects to a pdf, we're done.
         # = open repo http://hdl.handle.net/2060/20140010374
         if head_says_pdf(r):
-            if verbose:
-                print "the head says this is a PDF. we're quitting.", url
+            print "the head says this is a PDF. we're quitting.", url
             return True
 
-        # get the HTML tree and the bucket of words
-        page = r.text
+
+
+
+        # get the HTML tree
+        page = r.content
         page = page.replace("&nbsp;", " ")  # otherwise starts-with for lxml doesn't work
-        start_parsing = time()
         tree = html.fromstring(page)
-        if verbose:
-            print "parsing the html tree took {} for {}".format(elapsed(start_parsing), url)
 
-        useful_links = get_useful_links(tree)
-        try:
-            page_words = " ".join(tree.xpath("//body")[0].text_content().lower().split())
-        except IndexError:
-            # there is no body tag
-            page_words = ""
-
-
-
-        # tests that use the bucket of words
-
-        # reasons to reject outright
-        if page_says_closed(page_words, host, verbose):
-            return False
-
-
-        # tests that use the HTML tree
-
-        # if they are linking to a .docx or similar, this is probably open.
-        doc_link = find_doc_download_link(tree, verbose)
+        # if they are linking to a .docx or similar, this is open.
+        doc_link = find_doc_download_link(tree)
         if doc_link is not None:
-            if verbose :
-                print "found OA link target (non-pdf): ", get_link_target(doc_link, r.url)
+            print "found OA link target (non-pdf): ", get_link_target(doc_link, r.url)
             return True
 
-
-        # if a journal is linking to an open license, it's an open article
-        # = open journal http://doi.org/10.17528/cifor/004490
-        if host == "journal":
-            for link in useful_links:
-                if "creativecommons.org/licenses/" in link.href:
-                    if verbose:
-                        print "Found a CC-license for this journal article", link.anchor
-                    return True
-
-        # if a journal has a "purchase" link it's closed.
-            for link in useful_links:
-                link_blacklist = [
-                    # = closed journal http://www.sciencedirect.com/science/article/pii/S0147651300920050
-                    "purchase",
-
-                    # = closed journal http://link.springer.com/article/10.1007%2Fs10822-012-9571-0
-                    "get access"
-                ]
-                for bad_word in link_blacklist:
-                    if bad_word == link.anchor:
-                        if verbose:
-                            print "found a '{}' link in {}".format(bad_word, url)
-                        return False
-
-
-
-        # if they are linking to a PDF, we need to follow the link to make sure it's legit
-        pdf_download_link = find_pdf_link(tree, verbose)
-
+        pdf_download_link = find_pdf_link(tree)
         if pdf_download_link is not None:
-            target = get_link_target(pdf_download_link, r.url)
-            if verbose:
-                print "found OA link target: ", target, host
+            print "found OA link target: ", pdf_download_link.href, pdf_download_link.anchor
 
-            # = open journal http://www.emeraldinsight.com/doi/full/10.1108/00251740510597707
-            # = closed journal http://www.emeraldinsight.com/doi/abs/10.1108/14777261111143545
             if host == "journal":
-                if verbose:
-                    print "this is a journal. checking to see the PDF link actually gets a PDF"
-                return gets_a_pdf(target, verbose=verbose)
-            else:
-                return True
-        else:
-            if verbose:
-                print "found no PDF download link on ", url
+                print "this is a journal. checking to see the PDF link actually gets a PDF"
+                # if they are linking to a PDF, we need to follow the link to make sure it's legit
+                return gets_a_pdf(pdf_download_link, r.url)
 
+            else:  # host is "repo"
+                return True
+
+        print "found no PDF download link on ", url
         return False
 
 
+# = open journal http://www.emeraldinsight.com/doi/full/10.1108/00251740510597707
+# = closed journal http://www.emeraldinsight.com/doi/abs/10.1108/14777261111143545
+def gets_a_pdf(link, base_url):
+    if is_purchase_link(link):
+        return False
 
-def gets_a_pdf(url, verbose=False):
+    absolute_url = get_link_target(link, base_url)
     start = time()
-    with closing(requests.get(url, stream=True, timeout=5)) as r:
+    with closing(requests.get(absolute_url, stream=True, timeout=5)) as r:
         if head_says_pdf(r):
-            if verbose:
-                print "http header says this is a PDF. took {}s".format(elapsed(start))
-                print r.headers
+            print "http header says this is a PDF. took {}s".format(elapsed(start))
+            print r.headers
             return True
         else:
-            if verbose:
-                print "the http header says this ain't a PDF. took {}s".format(elapsed(start))
+            print "the http header says this ain't a PDF. took {}s".format(elapsed(start))
             return False
 
-def find_doc_download_link(tree, verbose=False):
-
-    # the top of this loop is copied from find_pdf_link()
-    links = tree.xpath("//a")
-    for link in links:
-        link_text = link.text_content().strip().lower()
-
-        try:
-            link_target = link.attrib["href"]
-        except KeyError:
-            # if the link doesn't point nowhere, it's no use to us
-            continue
-
-
-        # everything below is unique to this function.
+def find_doc_download_link(tree):
+    for link in get_useful_links(tree):
 
         # = open repo https://lirias.kuleuven.be/handle/123456789/372010
-        if ".docx" in link_target:
+        if ".doc" in link.href or ".doc" in link.anchor:
             return link
 
     return None
@@ -166,166 +101,103 @@ def get_useful_links(tree):
 
     for link in links:
         link_text = link.text_content().strip().lower()
-        try:
-            link_target = link.attrib["href"]
-        except KeyError:
+        if not link_text:
             continue
+        else:
+            link.anchor = link_text
 
-        link.anchor = link_text
-        link.href = link_target
+        if "href" not in link.attrib:
+            continue
+        else:
+            link.href = link.attrib["href"]
+
         ret.append(link)
 
     return ret
 
 
-def find_pdf_link(tree, verbose=False):
-    links = tree.xpath("//a")
-    for link in links:
-        link_text = link.text_content().strip().lower()
-        # if verbose:
-        #     print "trying with link text: ", link_text
+def is_purchase_link(link):
+    # = closed journal http://www.sciencedirect.com/science/article/pii/S0147651300920050
+    if "purchase" in link.anchor:
+        print "found a purchase link!", link.anchor, link.href
+        return True
 
-        try:
-            link_target = link.attrib["href"]
-        except KeyError:
-            # if the link doesn't point nowhere, it's no use to us
-            continue
+    return False
 
+def has_bad_anchor_word(anchor_text):
+    anchor_blacklist = [
+        # = closed repo https://works.bepress.com/ethan_white/27/
+        "user",
+        "guide",
 
-
-
-
-
-        """
-        The download link doesn't have PDF at the end, but the download button is nice and clear.
-
-        = open repo https://works.bepress.com/ethan_white/45/
-
-        = closed repo https://works.bepress.com/ethan_white/27/
-
-        = open repo http://ro.uow.edu.au/aiimpapers/269/
-        """
-        if link_text == "download":
-            return link
-
-
-        """
-        download text has the word "download" it is somewhere. This is in a seperate
-        block right now because we may need more precision on it (eg check the link_target for .pfd)
-
-        = open repo http://eprints.whiterose.ac.uk/77866/
-
-        note that researchgate can return various different things after the ? part of url.
-        makes for fussy testing but shouldn't matter much in production
-        = open repo https://www.researchgate.net/publication/235915359_Promotion_of_Virtual_Research_Communities_in_CHAIN
-        """
-        if "download" in link_text:
-            return link
-
-
-
-        """
-        download link anchor text is something like foobar.pdf
-
-        = open repo http://hdl.handle.net/1893/372
-
-        = open repo https://research-repository.st-andrews.ac.uk/handle/10023/7421
-        """
-        if len(re.findall(ur".\.pdf\b", link_text)):
-            return link
-
-
-        """
-        the link anchor text is just "PDF"
-
-        = open repo http://dro.dur.ac.uk/1241/
-        """
-        if link_text.lower() == "pdf":
-            return link
-
-        # = closed journal http://onlinelibrary.wiley.com/doi/10.1162/10881980152830079/abstract
-        if "get pdf" in link_text.lower():
-            return link
-
-
-
-        """
-        download link is identified with an image
-
-        # this test is no good, it keeps disappearing. find a better one:
-        open repo http://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.587.8827
-        """
-        for img in link.findall("img"):
-            try:
-                if "pdf" in img.attrib["src"].lower():
-                    return link
-            except KeyError:
-                continue  # no src attr
-
-    return None
-
-
-def url_leads_to_pdf(url):
-    pass
-
-def page_says_closed(page_words, host, verbose=False):
-
-    if host == "journal":
-        blacklist_phrases = [
-            # = closed journal http://www.cell.com/trends/genetics/abstract/S0168-9525(07)00023-6
-            "purchase access",
-
-            # = closed journal http://journals.aps.org/pra/abstract/10.1103/PhysRevA.81.052301
-            "buy article",
-            "subscription required",
-
-            # = closed journal http://er.uwpress.org/content/30/1/9
-            "purchase short-term access",
-            "this item requires a subscription",
-
-            # no tests for these yet
-            "purchase instant access"
-        ]
-    elif host == "repo":
-        blacklist_phrases = [
-
-            # = closed repo https://lirias.kuleuven.be/handle/123456789/9821
-            "request a copy",
-
-            "file restricted",
-            "full text not available",
-            "full text not currently available",
-            "full-text and supplementary files are not available",
-            "no files associated with this item",
-            "restricted to registered users",
-            "does not currently have the full-text",
-            "does not currently have full-text",
-            "does not have the full-text",
-            "does not have full-text",
-
-            # not sure if we should keep this one, danger of false negs
-            # = closed repo http://nora.nerc.ac.uk/8783/
-            "(login required)",
-
-            # = closed repo http://sro.sussex.ac.uk/54348/
-            # = closed repo http://researchbank.acu.edu.au/fea_pub/434/
-            "admin only"
-        ]
-
-    for phrase in blacklist_phrases:
-        if phrase in page_words:
-            if verbose:
-                print "{} is closed! We found the phrase '{}'".format(host, phrase)
+        # no examples for these yet
+        "supplement",
+        "figure",
+        "faq"
+    ]
+    for bad_word in anchor_blacklist:
+        if bad_word in anchor_text:
             return True
 
     return False
 
 
+def find_pdf_link(tree):
+    # tests we are not sure we want to run yet:
+    # if it has some semantic stuff in html head that says where the pdf is: that's the pdf.
+    # = open http://onlinelibrary.wiley.com/doi/10.1111/tpj.12616/abstract
+
+
+    for link in get_useful_links(tree):
+
+        # there are some links that are FOR SURE the pdf for this article
+        if has_bad_anchor_word(link.anchor):
+            continue
+
+
+        # the link HREF contains "pdf"
+        if "pdf" in link.href:
+            return link
+
+
+        # download link ANCHOR text is something like "manuscript.pdf" or like "PDF (1 MB)"
+        # = open repo http://hdl.handle.net/1893/372
+        # = open repo https://research-repository.st-andrews.ac.uk/handle/10023/7421
+        # = open repo http://dro.dur.ac.uk/1241/
+        if "pdf" in link.anchor:
+            return link
+
+
+        # button says download
+        # = open repo https://works.bepress.com/ethan_white/45/
+        # = open repo http://ro.uow.edu.au/aiimpapers/269/
+        # = open repo http://eprints.whiterose.ac.uk/77866/
+        if "download" in link.anchor:
+            if "citation" in link.anchor:
+                pass
+            else:
+                return link
+
+        # download link is identified with an image
+        for img in link.findall("img"):
+            try:
+                if "pdf" in img.attrib["src"].lower():
+                    return link
+            except KeyError:
+                pass  # no src attr
+
+        # = closed journal http://www.sciencedirect.com/science/article/pii/S0147651300920050
+        try:
+            if "pdf" in link.attrib["title"].lower():
+                return link
+        except KeyError:
+            pass
 
 
 
-def is_pdf_url(url):
-    return len(re.findall(ur"\.pdf\b", url)) > 0
+    return None
+
+
 
 def get_link_target(link_elem, base_url):
     try:
@@ -367,18 +239,17 @@ class Tests(object):
 
 
 class TestCase(object):
-    def __init__(self, oa_expected=False, host="repo", url=None, verbose=False):
+    def __init__(self, oa_expected=False, host="repo", url=None):
         self.expected = oa_expected
         self.host = host
         self.url = url
-        self.verbose = verbose
 
         self.elapsed = None
         self.result = None
 
     def run(self):
         my_start = time()
-        self.result = is_oa(self.url, self.host, self.verbose)
+        self.result = is_oa(self.url, self.host)
         self.elapsed = elapsed(my_start)
 
     @property
@@ -424,12 +295,9 @@ def get_test_cases():
         if "journal" in arg_list:
             my_test_case.host = "journal"
 
-        if "verbose" in arg_list:
-            my_test_case.verbose = True
 
         # immediately quit and return this one if the "only" flag is set
         if "only" in arg_list:
-            my_test_case.verbose = True
             return [my_test_case]
 
         # otherwise put this in the list and keep iterating
