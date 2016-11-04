43f7df9d4291a46314bac35bcc3c6ccc453edc42
Heather Piwowar <hpiwowar@gmail.com>
2016-11-03 18:16:08 -0700

    try unicode fix

--- put_base_records_in_elastic.py
+++ put_base_records_in_elastic.py
@@ -4,7 +4,26 @@ from time import sleep
 from util import elapsed
 import zlib
 import re
-from elasticsearch import Elasticsearch
+import json
+from elasticsearch import Elasticsearch, RequestsHttpConnection, serializer, compat, exceptions
+
+# from https://github.com/elastic/elasticsearch-py/issues/374
+# to work around unicode problem
+class JSONSerializerPython2(serializer.JSONSerializer):
+    """Override elasticsearch library serializer to ensure it encodes utf characters during json dump.
+    See original at: https://github.com/elastic/elasticsearch-py/blob/master/elasticsearch/serializer.py#L42
+    A description of how ensure_ascii encodes unicode characters to ensure they can be sent across the wire
+    as ascii can be found here: https://docs.python.org/2/library/json.html#basic-usage
+    """
+    def dumps(self, data):
+        # don't serialize strings
+        if isinstance(data, compat.string_types):
+            return data
+        try:
+            return json.dumps(data, default=self.default, ensure_ascii=True)
+        except (ValueError, TypeError) as e:
+            raise exceptions.SerializationError(data, e)
+
 
 class MissingTagException(Exception):
     pass
@@ -51,7 +70,7 @@ def main():
     # set up elasticsearch
     INDEX_NAME = "base"
     TYPE_NAME = "record"
-    es = Elasticsearch(os.getenv("ELASTICSEARCH_URL"))
+    es = Elasticsearch(os.getenv("ELASTICSEARCH_URL"), serializer=JSONSerializerPython2())
     if es.indices.exists(INDEX_NAME):
         print("deleting '%s' index..." % (INDEX_NAME))
         res = es.indices.delete(index = INDEX_NAME)
