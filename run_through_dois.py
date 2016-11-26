517904ece52c6acf2d89fdfff8a057248a04e025
Heather Piwowar <hpiwowar@gmail.com>
2016-11-26 15:25:44 -0800

    support reverse

--- run_through_dois.py
+++ run_through_dois.py
@@ -5,16 +5,21 @@ from publication import get_pub_from_biblio
 from util import elapsed
 
 # do this to get all env variables in console
-# set -o allexport
-# source conf-file
-# set +o allexport
+# source .env
 
-def run_through_dois(first=None, last=None, filename=None):
+def run_through_dois(first=None, last=None, filename=None, reverse=None):
     total_start = time()
     i = 0
     fh = open(filename, "r")
     has_more_records = True
-    for line in fh:
+
+    lines = fh.readlines()
+    if reverse:
+        print "reverse!"
+        lines.reverse()
+        i = -1 * len(lines)
+
+    for line in lines:
         doi = line.strip()
         my_pub = get_pub_from_biblio({"doi": doi}, force_refresh=False)
         print u"*** {}. {} {}".format(i, my_pub.doi, my_pub.has_fulltext_url)
@@ -31,6 +36,8 @@ if __name__ == "__main__":
     parser.add_argument('--filename', nargs="?", type=str, help="filename with dois, one per line")
     parser.add_argument('--first', nargs="?", type=str, help="first filename to process (example: --first ListRecords.14461")
     parser.add_argument('--last', nargs="?", type=str, help="last filename to process (example: --last ListRecords.14461)")
+    parser.add_argument('--reverse', nargs="?", default=False, type=bool,
+                            help="call to start at the bottom of the file and run through in reverse")
 
     parsed = parser.parse_args()
 
