847f454bf1c67d57625c89346c567ca3193b56d4
Heather Piwowar <hpiwowar@gmail.com>
2016-11-27 06:55:09 -0800

    change queries a bit

--- update_base_in_elastic.py
+++ update_base_in_elastic.py
@@ -129,11 +129,11 @@ def save_records_in_es(es, records_to_save, threads, chunk_size):
 
 # chosen to help randomize the documents
 query_strings = [
-    "review use correlation race disease cells",
-    "among correlates on cancer finding studies time anne mary jen james brown jackson",
-    "chan lee zhang william between role map relationship",
-    "one robert death children power andrew david",
-    "discourse about health analysis matthew high"
+    "review use correlations racial disease cell frank",
+    "among correlate on cancer finding study times anne mary jennifer john fred",
+    "chan lee zhang bill between role map relationships",
+    "one bill death adult power andrew daniel",
+    "discourse about health analyses matt low"
     ]
 
 query = {
