4544154487adf12dbd1b5e1206b5744cbcb302bf
Heather Piwowar <hpiwowar@gmail.com>
2017-02-15 09:38:53 -0800

    turn off reading from cache

--- publication.py
+++ publication.py
@@ -96,11 +96,13 @@ def get_pubs_from_biblio(biblios, force_refresh=False):
 
 def get_pub_from_biblio(biblio, force_refresh=False):
 
-    my_pub = None
-    if not force_refresh:
-        my_pub = lookup_product_in_cache(**biblio)
-        if my_pub and my_pub.has_been_run:
-            return my_pub
+
+    ### don't lookup things in cache right now
+    # my_pub = None
+    # if not force_refresh:
+    #     my_pub = lookup_product_in_cache(**biblio)
+    #     if my_pub and my_pub.has_been_run:
+    #         return my_pub
 
     my_pub = build_publication(**biblio)
     my_pub.refresh()
