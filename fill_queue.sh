015067d7beeb9ca0d10eb524e60acd048dd163df
Heather Piwowar <hpiwowar@gmail.com>
2017-03-10 11:38:11 -0800

    10mins worth

--- fill_queue.sh
+++ fill_queue.sh
@@ -1,9 +1,4 @@
-python update.py Crossref.run --limit=100000 --chunk=50 --rq
-python update.py Crossref.run --limit=100000 --chunk=50 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=50 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=50 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=50 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=50 --rq --append
+python update.py Crossref.run --limit=10000 --chunk=50 --rq
 python update.py Crossref.run --limit=100000 --chunk=50 --rq --append
 python update.py Crossref.run --limit=100000 --chunk=50 --rq --append
 python update.py Crossref.run --limit=100000 --chunk=50 --rq --append
