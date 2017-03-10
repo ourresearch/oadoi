56e0f829abb4f8a754f7876d4e590b9a46e33927
Heather Piwowar <hpiwowar@gmail.com>
2017-03-10 11:21:24 -0800

    add more jobs

--- fill_queue.sh
+++ fill_queue.sh
@@ -1,6 +1,10 @@
-python update.py Crossref.run --limit=10000 --chunk=50 --rq
-python update.py Crossref.run --limit=10000 --chunk=50 --rq --append
-python update.py Crossref.run --limit=10000 --chunk=50 --rq --append
-python update.py Crossref.run --limit=10000 --chunk=50 --rq --append
-python update.py Crossref.run --limit=10000 --chunk=50 --rq --append
-python update.py Crossref.run --limit=10000 --chunk=50 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=50 --rq
+python update.py Crossref.run --limit=100000 --chunk=50 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=50 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=50 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=50 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=50 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=50 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=50 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=50 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=50 --rq --append
