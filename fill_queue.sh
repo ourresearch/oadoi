5317613a3e88d95918a48b5e56ea181336accfff
Heather Piwowar <hpiwowar@gmail.com>
2017-03-14 10:39:24 -0700

    try chunk size 250

--- fill_queue.sh
+++ fill_queue.sh
@@ -1,11 +1,11 @@
-python update.py Crossref.run --limit=10000 --chunk=100 --rq
-python update.py Crossref.run --limit=100000 --chunk=100 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=100 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=100 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=100 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=100 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=100 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=100 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=100 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=100 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=100 --rq --append
+python update.py Crossref.run --limit=10000 --chunk=250 --rq
+python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
