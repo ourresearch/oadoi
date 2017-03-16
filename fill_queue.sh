74018beebe2f3f640a7bf78b80339fa503d1922f
Heather Piwowar <hpiwowar@gmail.com>
2017-03-15 22:47:41 -0700

    one more time

--- fill_queue.sh
+++ fill_queue.sh
@@ -1,14 +1,8 @@
-python update.py Crossref.run --limit=10000 --chunk=1 --rq
-python update.py Crossref.run --limit=100000 --chunk=1 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=1 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=1 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=1 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=1 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=1 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=1 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=1 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=1 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=1 --rq --append
+python update.py Crossref.run --limit=10000 --chunk=10 --rq
+python update.py Crossref.run --limit=100000 --chunk=10 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=10 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=10 --rq --append
+python update.py Crossref.run --limit=100000 --chunk=10 --rq --append
 
 #python update.py Crossref.run --limit=10000 --chunk=250 --rq
 #python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
