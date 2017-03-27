b6b66d4f9ee8b5f56d69958ff1f696ed45519a08
Heather Piwowar <hpiwowar@gmail.com>
2017-03-27 09:03:15 -0700

    update fill_queue

--- fill_queue.sh
+++ fill_queue.sh
@@ -1,17 +1,21 @@
-python update.py Crossref.run --limit=10000 --chunk=10 --rq
-python update.py Crossref.run --limit=100000 --chunk=10 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=10 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=10 --rq --append
-python update.py Crossref.run --limit=100000 --chunk=10 --rq --append
+#!/bin/bash
+trap "exit" INT
+
+python update.py Crossref.run_if_open --limit=1000 --rq
+for (( i=1; i <= 1000000; i++ ))
+do
+    echo "loop $i"
+    python update.py Crossref.run_if_open --limit=1000 --rq --append
+done
+
+
+
+
+
+#python update.py Crossref.run --limit=10000 --chunk=10 --rq
+#python update.py Crossref.run --limit=100000 --chunk=10 --rq --append
+#python update.py Crossref.run --limit=100000 --chunk=10 --rq --append
 
 #python update.py Crossref.run --limit=10000 --chunk=250 --rq
-#python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
-#python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
-#python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
-#python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
-#python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
-#python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
-#python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
-#python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
 #python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
 #python update.py Crossref.run --limit=100000 --chunk=250 --rq --append
