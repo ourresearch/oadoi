2bb97f5ce72da92c63db50c7d03ff0e55df47e47
Heather Piwowar <hpiwowar@gmail.com>
2017-06-22 09:38:46 -0700

    fix sh

--- run_worker.sh
+++ run_worker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # dyno number avail in $DYNO as per http://stackoverflow.com/questions/16372425/can-you-programmatically-access-current-heroku-dyno-id-name/16381078#16381078
 
-for ((i=1; i<=5; i++))
+for i in {1..5}
 do
   COMMAND="python update.py Crossref.run_with_hybrid --chunk=5 --limit=100000000 --name=hybrid-$DYNO:${i} "
   echo $COMMAND
