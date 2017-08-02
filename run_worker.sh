a710a8e83cd853ba670557a8f3d365c7f04d78fb
Heather Piwowar <hpiwowar@gmail.com>
2017-08-02 10:37:12 -0700

    increase the chunk size

--- run_worker.sh
+++ run_worker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # dyno number avail in $DYNO as per http://stackoverflow.com/questions/16372425/can-you-programmatically-access-current-heroku-dyno-id-name/16381078#16381078
 
-for (( i=1; i<=5; i++ ))
+for (( i=1; i<=8; i++ ))
 do
-  COMMAND="python update.py Crossref.run --chunk=10 --limit=100000000 --name=run-$DYNO:${i} "
+  COMMAND="python update.py Crossref.run --chunk=100 --limit=100000000 --name=run-$DYNO:${i} "
   echo $COMMAND
   $COMMAND&
 done
