dddcbe90cae9c77aa2a17a48cb794f3f8784d3d0
Heather Piwowar <hpiwowar@gmail.com>
2017-08-23 17:15:43 -0700

    add run_dates_workier.sh

---
+++ run_dates_worker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# dyno number avail in $DYNO as per http://stackoverflow.com/questions/16372425/can-you-programmatically-access-current-heroku-dyno-id-name/16381078#16381078
+
+for (( i=1; i<=2; i++ ))
+do
+  COMMAND="python update.py DateRange.get_crossref_api_raw --chunk=1 --limit=100000000 --name=hybrid-$DYNO:${i} "
+  echo $COMMAND
+  $COMMAND&
+done
+trap "kill 0" INT TERM EXIT
+wait
