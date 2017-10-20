454a886560fb7abd1c271e6bc7ba17761de13405
Heather Piwowar <hpiwowar@gmail.com>
2017-10-20 14:49:55 +0100

    8 per runner

--- run_green.sh
+++ run_green.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # dyno number avail in $DYNO as per http://stackoverflow.com/questions/16372425/can-you-programmatically-access-current-heroku-dyno-id-name/16381078#16381078
 
-for (( i=1; i<=1; i++ ))
+for (( i=1; i<=8; i++ ))
 do
   COMMAND="python green_queue.py --chunk=10 --limit=100000000 --run"
   echo $COMMAND
