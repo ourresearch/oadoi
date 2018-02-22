ca2c36221d242e93886f13cb267b2cba60bcfd25
Heather Piwowar <hpiwowar@gmail.com>
2018-02-22 06:59:40 -0800

    focus on osf for now

--- queue_page.py
+++ queue_page.py
@@ -46,8 +46,9 @@ class DbQueueRepo(DbQueue):
                    SELECT *
                    FROM   {queue_table}
                    WHERE  started is null and num_pub_matches is null
-                   and rand > {rand_thresh}
+                   -- and rand > {rand_thresh}
                    and repo_id not in ('quod.lib.umich.edu/cgi/o/oai/oai')
+                   and repo_id in (select id from repository where name='OSF Preprints')
                    ORDER BY rand
                LIMIT  {chunk}
                FOR UPDATE SKIP LOCKED
