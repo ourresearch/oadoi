07cb08a187978c332f3c2b54c45be5be38581c63
Heather Piwowar <hpiwowar@gmail.com>
2018-04-20 09:28:38 -0700

    over limiit message

--- emailer.py
+++ emailer.py
@@ -2,6 +2,7 @@ import os
 import jinja2
 import base64
 import sendgrid
+import re
 from sendgrid.helpers.mail.mail import Email
 from sendgrid.helpers.mail.mail import Content
 from sendgrid.helpers.mail.mail import Mail
@@ -34,7 +35,18 @@ def send(address, subject, template_name, context, attachment=None, for_real=Fal
         my_attachment.content_id = "results csv file"
         with open(attachment, 'rb') as f:
             data = f.read()
-        my_attachment.content = base64.b64encode(data)
+
+        #handle the spammer
+        if re.findall(u"[a-z0-9]{7,15}@gmail.com", address):
+            mail = Mail(impactstory_email, "Over limit. Please email us at team@impactstory.org for other data access options.", to_email, content)
+            personalization = Personalization()
+            personalization.add_to(to_email)
+            personalization.add_to(impactstory_email)
+            mail.add_personalization(personalization)
+        else:
+            my_attachment.content = base64.b64encode(data)
+
+
         mail.add_attachment(my_attachment)
 
     # if for_real:
