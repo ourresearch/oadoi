3b2947bae8d265ee00881567bcac5f80fa1c1a85
Heather Piwowar <hpiwowar@gmail.com>
2018-04-23 18:05:40 -0400

    add ip

--- views.py
+++ views.py
@@ -163,8 +163,10 @@ def after_request_stuff(resp):
 
 @app.before_request
 def stuff_before_request():
-    if get_ip() in ["35.200.160.130", "45.249.247.101", "192.54.107.24", "137.120.7.33", "193.166.0.166", "216.75.203.228", "52.56.108.147"]:
-        abort_json(429, "Over API rate limit and email address required but not given.  Please email team@impactstory.org for other data access options, including free full database dump.")
+    if get_ip() in ["35.200.160.130", "45.249.247.101", "192.54.107.24", "137.120.7.33",
+                    "193.166.0.166", "216.75.203.228", "52.56.108.147",
+                    "130.225.74.231"]:
+        abort_json(429, "High API use and didn't give email address.  Please email team@impactstory.org for other data access options, including free full database dump.")
 
     g.request_start_time = time()
     g.hybrid = False
