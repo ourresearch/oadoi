3299b8140987083fd7f152f6114d8a20c6242864
Heather Piwowar <hpiwowar@gmail.com>
2018-05-14 11:14:03 -0700

    concentrate on zenodo for a bit

--- queue_page.py
+++ queue_page.py
@@ -47,7 +47,9 @@ class DbQueueRepo(DbQueue):
                    FROM   {queue_table}
                    WHERE  started is null and num_pub_matches is null
                    -- and rand > {rand_thresh}
-                   and repo_id not in ('quod.lib.umich.edu/cgi/o/oai/oai')
+                   # and updated > '2018-05-01'::timestamp
+                   # and repo_id not in ('quod.lib.umich.edu/cgi/o/oai/oai')
+                   and repo_id in ('zenodo.org/oai2d')
                    ORDER BY rand
                LIMIT  {chunk}
                FOR UPDATE SKIP LOCKED
