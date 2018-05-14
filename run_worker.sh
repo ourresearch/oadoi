002b33d61e157e3d0558e18c23bba44103ad2c57
Heather Piwowar <hpiwowar@gmail.com>
2018-05-13 20:31:23 -0700

    just use standard database_url

--- run_worker.sh
+++ run_worker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # dyno number avail in $DYNO as per http://stackoverflow.com/questions/16372425/can-you-programmatically-access-current-heroku-dyno-id-name/16381078#16381078
 
+
 for (( i=1; i<=2; i++ ))
 do
 #   COMMAND="python queue_pub.py --run --name=run-$DYNO:${i} "
