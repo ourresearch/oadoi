5c199fd3f07a4b86b0352992ddaa7038fb53e6ae
Heather Piwowar <hpiwowar@gmail.com>
2018-05-15 18:56:53 -0700

    10 at a time, 3 processes

--- run_page.sh
+++ run_page.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # dyno number avail in $DYNO as per http://stackoverflow.com/questions/16372425/can-you-programmatically-access-current-heroku-dyno-id-name/16381078#16381078
 
-for (( i=1; i<=1; i++ ))
+for (( i=1; i<=3; i++ ))
 do
-   COMMAND="python queue_page.py --run"
+   COMMAND="python queue_page.py --run --chunk=10"
   echo $COMMAND
   $COMMAND&
 done
