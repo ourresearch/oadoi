d94248b4b27d2658c260e70e9084f1061e7c3722
Heather Piwowar <hpiwowar@gmail.com>
2018-05-29 18:04:56 -0700

    back to 15

--- run_hybrid_worker.sh
+++ run_hybrid_worker.sh
@@ -2,7 +2,7 @@
 # dyno number avail in $DYNO as per http://stackoverflow.com/questions/16372425/can-you-programmatically-access-current-heroku-dyno-id-name/16381078#16381078
 
 # for (( i=1; i<=15; i++ ))
-for (( i=1; i<=5; i++ ))
+for (( i=1; i<=15; i++ ))
 do
   COMMAND="python queue_pub.py --run --method=refresh --chunk=1"
   echo $COMMAND
