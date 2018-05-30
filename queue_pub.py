b0db5cef3b5dd5b402a95399b7701eebd261cb83
Heather Piwowar <hpiwowar@gmail.com>
2018-05-29 18:38:52 -0700

    no order by rand

--- queue_pub.py
+++ queue_pub.py
@@ -46,7 +46,7 @@ class DbQueuePub(DbQueue):
                    WHERE  started is null
                    AND scrape_updated is null
                    and exists (select 1 from chorus where chorus.id=pub.id)
-                   order by rand desc
+                   -- order by rand desc
                    LIMIT  {chunk}
                    FOR UPDATE SKIP LOCKED
                    )
