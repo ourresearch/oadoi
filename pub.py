b5de6bb13ae0c27d58b9d525b9f383581b428668
Richard Orr <richard@impactstory.org>
2019-02-24 13:18:49 -0800

    exclude open locations with bad pdf urls

--- pub.py
+++ pub.py
@@ -28,7 +28,7 @@ from pmh_record import PmhRecord
 from pmh_record import title_is_too_common
 from pmh_record import title_is_too_short
 import oa_manual
-from open_location import OpenLocation
+from open_location import OpenLocation, validate_pdf_urls
 from reported_noncompliant_copies import reported_noncompliant_url_fragments
 from webpage import PublisherWebpage
 # from abstract import Abstract
@@ -1333,15 +1333,24 @@ class Pub(db.Model):
         return locations
 
     @property
-    def sorted_locations(self):
+    def filtered_locations(self):
         locations = self.open_locations
+
+        # now remove noncompliant ones
+        locations = [location for location in locations if not location.is_reported_noncompliant]
+
+        validate_pdf_urls(locations)
+        locations = [location for location in locations if location.pdf_url_valid]
+
+        return locations
+
+    @property
+    def sorted_locations(self):
+        locations = self.filtered_locations
         # first sort by best_url so ties are handled consistently
         locations = sorted(locations, key=lambda x: x.best_url, reverse=False)
         # now sort by what's actually better
         locations = sorted(locations, key=lambda x: x.sort_score, reverse=False)
-
-        # now remove noncompliant ones
-        locations = [location for location in locations if not location.is_reported_noncompliant]
         return locations
 
     @property
