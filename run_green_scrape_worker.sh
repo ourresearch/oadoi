61e90ff5b81449a355fe52a08a5d0bb01295a7e6
Richard Orr <richard@impactstory.org>
2019-03-20 21:13:46 -0700

    background green scrape workers

--- run_green_scrape_worker.sh
+++ run_green_scrape_worker.sh
@@ -5,7 +5,7 @@ for (( i=1; i<=$GREEN_SCRAPE_WORKERS_PER_DYNO; i++ ))
 do
   COMMAND="python queue_green_oa_scrape.py --run --chunk=$GREEN_SCRAPE_CHUNK_SIZE"
   echo $COMMAND
-  $COMMAND
+  $COMMAND &
 done
 trap "kill 0" INT TERM EXIT
 wait
