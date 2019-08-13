09db4995de6bbdb456d4fcf0e9359c6eaa3aa34f
Richard Orr <richard@impactstory.org>
2019-08-12 22:45:34 -0400

    add a too-common title

--- pmh_record.py
+++ pmh_record.py
@@ -89,6 +89,7 @@ def title_is_too_common(normalized_title):
         deepbrainstimulationparkinsonsdisease
         mathematicalmorphologyanditsapplicationssignalandimageprocessing
         principalcomponentanalysis
+        acuterespiratorydistresssyndrome
         """
     for common_title in common_title_string.split("\n"):
         if normalized_title==common_title.strip():
