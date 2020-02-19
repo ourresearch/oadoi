69cf540fb3deed53ff98e39edb4704891b6aea27
Richard Orr <richard@impactstory.org>
2020-02-19 12:47:32 -0500

    add a too-common pmh title

--- pmh_record.py
+++ pmh_record.py
@@ -99,6 +99,7 @@ def title_is_too_common(normalized_title):
         poincareandthreebodyproblem
         systemiclupuserythematosus
         bayeractivitiesdailylivingscalebadl
+        mineralogicalsocietyamerica
         """
     for common_title in common_title_string.split("\n"):
         if normalized_title == common_title.strip():
