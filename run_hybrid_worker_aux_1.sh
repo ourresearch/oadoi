f7a36fd0b0782b20dc3d97f700b6b4cbf6ecfa19
Richard Orr <richard@impactstory.org>
2021-02-09 12:41:37 -0500

    create a second aux refresh process

---
+++ run_hybrid_worker_aux_1.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# dyno number avail in $DYNO as per http://stackoverflow.com/questions/16372425/can-you-programmatically-access-current-heroku-dyno-id-name/16381078#16381078
+
+for (( i=1; i<=$PUB_REFRESH_WORKERS_PER_DYNO; i++ ))
+do
+  COMMAND="python queue_pub_refresh_aux.py --run --queue=1 --chunk=$PUB_REFRESH_CHUNK_SIZE"
+  echo $COMMAND
+  $COMMAND
+done
+trap "kill 0" INT TERM EXIT
+wait
