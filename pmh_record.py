312d97ed2f2ff848beeecdfbe72a64005f9ff736
Richard Orr <richard@impactstory.org>
2021-02-10 17:23:59 -0500

    add a too-common pmh title

--- pmh_record.py
+++ pmh_record.py
@@ -123,6 +123,7 @@ def title_is_too_common(normalized_title):
         artificialintelligence
         mergersandacquisitions
         coronavirusdiseasecovid
+        ethicsscientificpublishing
         """
     for common_title in common_title_string.split("\n"):
         if normalized_title == common_title.strip():
