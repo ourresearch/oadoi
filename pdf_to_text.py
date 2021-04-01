d8d8c885f75924423b0361ea0711c342c165ac5c
Richard Orr <richard@impactstory.org>
2021-04-01 14:40:52 -0400

    add an accepted version label

--- pdf_to_text.py
+++ pdf_to_text.py
@@ -6,6 +6,10 @@ from app import logger
 
 
 def convert_pdf_to_txt(r, max_pages=3):
+    return '\n'.join(convert_pdf_to_txt_pages(r, max_pages))
+
+
+def convert_pdf_to_txt_pages(r, max_pages=3):
     if r.status_code != 200:
         logger.info(u"error: status code {} in convert_pdf_to_txt".format(r.status_code))
         return None
@@ -14,6 +18,4 @@ def convert_pdf_to_txt(r, max_pages=3):
         r.encoding = "utf-8"
     fp = StringIO(r.content_big())
 
-    text = '\n'.join(itertools.islice(pdftotext.PDF(fp), max_pages))
-
-    return text
+    return list(itertools.islice(pdftotext.PDF(fp), max_pages))
