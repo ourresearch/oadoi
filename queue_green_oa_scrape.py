befa8fce008862e0abc792445d85c57cb5076b9e
Richard Orr <richard@impactstory.org>
2021-05-14 15:16:41 -0400

    don't use nested processes in green scrape queue

--- queue_green_oa_scrape.py
+++ queue_green_oa_scrape.py
@@ -36,7 +36,7 @@ def scrape_pages(pages):
     db.engine.dispose()
 
     pool = get_worker_pool()
-    map_results = pool.map(scrape_with_timeout, pages, chunksize=1)
+    map_results = pool.map(scrape_page, pages, chunksize=1)
     scraped_pages = [p for p in map_results if p]
     logger.info('finished scraping all pages')
     pool.close()
@@ -57,40 +57,9 @@ def scrape_pages(pages):
     return scraped_page_ids
 
 
-# need to spawn processes from workers but can't do that if worker is daemonized
-class NDProcess(Process):
-    def _get_daemon(self):
-        return False
-
-    def _set_daemon(self, value):
-        pass
-
-    daemon = property(_get_daemon, _set_daemon)
-
-
-class NDPool(multiprocessing.pool.Pool):
-    Process = NDProcess
-
-
 def get_worker_pool():
     num_request_workers = int(os.getenv('GREEN_SCRAPE_PROCS_PER_WORKER', 10))
-    return NDPool(processes=num_request_workers, maxtasksperchild=10)
-
-
-# Pool.map hangs if a worker process dies, so wrap the scrape in a new process and watch that
-def scrape_with_timeout(page):
-    pool = NDPool(processes=1)
-    async_result = pool.apply_async(scrape_page, (page,))
-    result = None
-    try:
-        result = async_result.get(timeout=600)
-        pool.close()
-    except TimeoutError:
-        logger.info('page scrape timed out: {}'.format(page))
-        pool.terminate()
-
-    pool.join()
-    return result
+    return multiprocessing.pool.Pool(processes=num_request_workers, maxtasksperchild=10)
 
 
 def scrape_page(page):
