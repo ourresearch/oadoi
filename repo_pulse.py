8d91f5eba13e6f82f0a5c4990eaa364e923d1b15
Richard Orr <richard@impactstory.org>
2021-05-27 14:42:14 -0400

    correct numeric types in repo pulse view

--- repo_pulse.py
+++ repo_pulse.py
@@ -10,12 +10,12 @@ class BqRepoPulse(db.Model):
     check0_identify_status = db.Column(db.Text)
     check1_query_status = db.Column(db.Text)
     last_harvested = db.Column(db.DateTime)
-    num_distinct_pmh_records = db.Column(db.Text)
-    num_distinct_pmh_records_matching_dois = db.Column(db.Text)
-    num_distinct_pmh_records_matching_dois_with_fulltext = db.Column(db.Text)
-    num_distinct_pmh_submitted_version = db.Column(db.Text)
-    num_distinct_pmh_accepted_version = db.Column(db.Text)
-    num_distinct_pmh_published_version = db.Column(db.Text)
+    num_distinct_pmh_records = db.Column(db.Numeric)
+    num_distinct_pmh_records_matching_dois = db.Column(db.Numeric)
+    num_distinct_pmh_records_matching_dois_with_fulltext = db.Column(db.Numeric)
+    num_distinct_pmh_submitted_version = db.Column(db.Numeric)
+    num_distinct_pmh_accepted_version = db.Column(db.Numeric)
+    num_distinct_pmh_published_version = db.Column(db.Numeric)
     error = db.Column(db.Text)
 
     def __repr__(self):
@@ -34,15 +34,15 @@ class BqRepoPulse(db.Model):
         results["status"] = {
             "check0_identify_status": self.check0_identify_status,
             "check1_query_status": self.check1_query_status,
-            "num_pmh_records": self.num_distinct_pmh_records,
+            "num_pmh_records": int(self.num_distinct_pmh_records or 0),
             "last_harvest": self.last_harvested,
-            "num_pmh_records_matching_dois": self.num_distinct_pmh_records_matching_dois,
-            "num_pmh_records_matching_dois_with_fulltext": self.num_distinct_pmh_records_matching_dois_with_fulltext
+            "num_pmh_records_matching_dois": int(self.num_distinct_pmh_records_matching_dois or 0),
+            "num_pmh_records_matching_dois_with_fulltext": int(self.num_distinct_pmh_records_matching_dois_with_fulltext or 0)
         }
         results["by_version_distinct_pmh_records_matching_dois"] = {
-            "submittedVersion": self.num_distinct_pmh_submitted_version,
-            "acceptedVersion": self.num_distinct_pmh_accepted_version,
-            "publishedVersion": self.num_distinct_pmh_published_version
+            "submittedVersion": int(self.num_distinct_pmh_submitted_version or 0),
+            "acceptedVersion": int(self.num_distinct_pmh_accepted_version or 0),
+            "publishedVersion": int(self.num_distinct_pmh_published_version or 0)
         }
 
         return results
