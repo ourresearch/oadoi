e32a84a53ded8021c7b6c82c4f4d2d63f467caf7
Richard Orr <richard@impactstory.org>
2021-09-15 11:26:34 -0400

    look up archive s3 keys by page id

--- works_db/pmh_record_location.py
+++ works_db/pmh_record_location.py
@@ -1,7 +1,10 @@
+import datetime
+from urllib.parse import quote
+from urllib.parse import urlparse
+
 from app import db
 from works_db.location import Location
-from urllib.parse import urlparse
-from pmh_record import oai_tag_match
+
 
 class PmhRecordLocation(Location):
     __tablename__ = None
@@ -14,19 +17,42 @@ class PmhRecordLocation(Location):
 
     @staticmethod
     def from_pmh_record(pmh_record):
+        best_page = PmhRecordLocation.best_page(pmh_record)
+        if not best_page:
+            return None
+
         location = PmhRecordLocation.query.filter(PmhRecordLocation.pmh_id == pmh_record.id).scalar()
 
         if not location:
             location = PmhRecordLocation()
 
+        location.pmh_id = pmh_record.id
+
         location.title = pmh_record.title
-        location.authors = pmh_record.authors
+        location.authors = [{"raw": author} for author in pmh_record.authors]
         location.doi = pmh_record.doi
 
-        best_page = PmhRecordLocation.best_page(pmh_record)
         location.record_webpage_url = best_page.url
-        location.record_webpage_archive_url = best_page.landing_page_archive_url
+        location.record_webpage_archive_url = best_page.landing_page_archive_url()
         location.record_structured_url = best_page.get_pmh_record_url()
+        location.record_structured_archive_url = f'https://api.unpaywall.org/pmh_record_xml/{quote(pmh_record.id)}'
+
+        location.work_pdf_url = best_page.scrape_pdf_url
+        location.work_pdf_archive_url = best_page.fulltext_pdf_archive_url()
+        location.is_work_pdf_url_free_to_read = best_page.scrape_pdf_url and True
+
+        location.is_oa = bool(best_page.is_open)
+
+        location.oa_date = datetime.datetime.combine(
+            best_page.first_available,
+            datetime.datetime.min.time()
+        )
+
+        location.open_license = best_page.scrape_license
+        location.open_version = best_page.scrape_version
+
+        if db.session.is_modified(location):
+            location.updated = datetime.datetime.utcnow().isoformat()
 
         return location
 
