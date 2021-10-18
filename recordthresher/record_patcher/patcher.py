f02f7d7f74546559464b5b47e29e39257b99745d
Richard Orr <richard@impactstory.org>
2021-10-18 12:53:05 -0400

    retry parseland calls

--- recordthresher/record_patcher/patcher.py
+++ recordthresher/record_patcher/patcher.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from time import sleep
 
 import requests
 from lxml import etree
@@ -68,32 +69,54 @@ class PmhRecordPatcher(RecordPatcher):
 
     @classmethod
     def _parseland_authors(cls, repo_page):
-        parseland_response = requests.get(f'https://parseland.herokuapp.com/parse-repository?page-id={repo_page.id}')
+        retry = True
+        next_retry_interval = 1
+        cumulative_wait = 0
+        max_cumulative_wait = 10
 
-        if parseland_response.ok:
-            try:
-                parseland_json = parseland_response.json()
-                message = parseland_json.get('message', None)
+        while retry and cumulative_wait < max_cumulative_wait:
+            parseland_response = requests.get(f'https://parseland.herokuapp.com/parse-repository?page-id={repo_page.id}')
 
-                if not isinstance(message, list):
+            if parseland_response.ok:
+                logger.info('got a 200 response from parseland')
+                try:
+                    parseland_json = parseland_response.json()
+                    message = parseland_json.get('message', None)
+
+                    if not isinstance(message, list):
+                        logger.error("message isn't a list")
+                        return None
+
+                    authors = []
+                    logger.error(f'got {len(message)} authors')
+
+                    for pl_author in message:
+                        author = {'raw': pl_author.get('name'), 'affiliation': []}
+                        pl_affiliations = pl_author.get('affiliations')
+
+                        if isinstance(pl_affiliations, list):
+                            for pl_affiliation in pl_affiliations:
+                                author['affiliation'].append({'name': pl_affiliation})
+
+                        authors.append(normalize_author(author))
+
+                    return authors
+                except ValueError as e:
+                    logger.error("response isn't valid json")
                     return None
+            else:
+                logger.warning(f'got error response from parseland: {parseland_response}')
 
-                authors = []
+                if parseland_response.status_code == 404 and 'Source file not found' in parseland_response.text:
+                    logger.info(f'retrying in {next_retry_interval} seconds')
+                    sleep(next_retry_interval)
+                    cumulative_wait += next_retry_interval
+                    next_retry_interval *= 1.5
+                else:
+                    logger.info('not retrying')
+                    retry = False
 
-                for pl_author in message:
-                    author = {'raw': pl_author.get('name'), 'affiliation': []}
-                    pl_affiliations = pl_author.get('affiliations')
-
-                    if isinstance(pl_affiliations, list):
-                        for pl_affiliation in pl_affiliations:
-                            author['affiliation'].append({'name': pl_affiliation})
-
-                    authors.append(normalize_author(author))
-
-                return authors
-            except ValueError as e:
-                return None
-
+        logger.info(f'done retrying after {cumulative_wait} seconds')
         return None
 
 
