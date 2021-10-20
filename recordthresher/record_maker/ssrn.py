a02b8b6d6c59c266987ee8d29957338aad4fe567
Richard Orr <richard@impactstory.org>
2021-10-20 13:40:44 -0400

    get ssrn authors from parseland

---
+++ recordthresher/record_maker/ssrn.py
@@ -0,0 +1,13 @@
+from recordthresher.record_maker import CrossrefRecordMaker
+from recordthresher.util import parseland_authors
+
+
+class SsrnRecordMaker(CrossrefRecordMaker):
+    @staticmethod
+    def _is_specialized_record_maker(pub):
+        return pub.doi.startswith('10.2139/ssrn.')
+
+    @classmethod
+    def _make_source_specific_record_changes(cls, record, pub):
+        if (pl_authors := parseland_authors(cls._parseland_api_url(pub))) is not None:
+            record.authors = pl_authors
