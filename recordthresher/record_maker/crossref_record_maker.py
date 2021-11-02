084c96e45c59f6df0b38b20ae5f2e139e7340206
Richard Orr <richard@impactstory.org>
2021-11-02 12:58:27 -0400

    get ACS affiliations from parseland

--- recordthresher/record_maker/crossref_record_maker.py
+++ recordthresher/record_maker/crossref_record_maker.py
@@ -135,12 +135,15 @@ class CrossrefRecordMaker(RecordMaker):
 
     @classmethod
     def _make_source_specific_record_changes(cls, record, pub):
-        if pub.publisher and any(p in pub.publisher for p in [
-            'Elsevier',
-            'Springer Science and Business Media',
-            'IEEE',
-            'MDPI AG',
-            'Springer International Publishing',
-            'IOP Publishing',
-        ]):
-            cls._append_parseland_affiliations(record, pub)
+        if not any(author.get('affiliation') for author in record.authors):
+            if pub.publisher and any(p in pub.publisher for p in [
+                'Elsevier',
+                'Springer Science and Business Media',
+                'IEEE',
+                'MDPI AG',
+                'Springer International Publishing',
+                'IOP Publishing',
+                'Ovid Technologies (Wolters Kluwer Health)',
+                'American Chemical Society',
+            ]):
+                cls._append_parseland_affiliations(record, pub)
