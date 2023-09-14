54a5fc0bcc796c7edb7cb1cdf94daa8e86de9bf1
nolan <nolanm1995@live.com>
2023-09-14 15:00:21 -0500

    Update fulltext insert stmnt

--- pdf_parse.py
+++ pdf_parse.py
@@ -177,9 +177,9 @@ def save_grobid_response_loop(pdf_doi_q: Queue, db_q: Queue):
                 continue
             parsed = fetch_parsed_pdf_response(doi)['message']
             stmnt = text(
-                'UPDATE ins.recordthresher_record SET fulltext = :fulltext WHERE doi = :doi').bindparams(
-                fulltext=parsed['fulltext'],
-                doi=doi)
+                '''INSERT INTO mid.record_fulltext (recordthresher_id, fulltext) SELECT r.id, :fulltext FROM (SELECT id FROM ins.recordthresher_record WHERE doi = :doi AND record_type = 'crossref_doi') r ON CONFLICT(recordthresher_id) DO UPDATE SET fulltext = :fulltext;''').bindparams(
+                    fulltext=parsed['fulltext'],
+                    doi=doi)
             db_q.put((stmnt, 'OPENALEX', False))
             other_obj = {}
             for k, v in parsed.items():
