a4ab244ed64a270317f7f78b931c1b7b1fdcd4f1
nolan <nolanm1995@live.com>
2023-10-24 14:47:40 -0500

    Fix s3_conn, reformat/refactor Move repo PDF download to page.py

--- pdf_util.py
+++ pdf_util.py
@@ -1,8 +1,10 @@
 from enum import Enum
 from urllib.parse import quote
 
-import boto3
 import botocore
+from sqlalchemy import text
+
+from app import s3_conn, logger, db
 
 from const import PDF_ARCHIVE_BUCKET, GROBID_XML_BUCKET
 
@@ -36,18 +38,40 @@ class PDFVersion(Enum):
 
     def in_s3(self, doi) -> bool:
         try:
-            self.s3.get_object(Bucket=PDF_ARCHIVE_BUCKET, Key=self.s3_key(doi))
+            s3_conn.get_object(Bucket=PDF_ARCHIVE_BUCKET, Key=self.s3_key(doi))
             return True
         except botocore.exceptions.ClientError as e:
             return False
 
     def grobid_in_s3(self, doi):
         try:
-            self.s3.get_object(Bucket=GROBID_XML_BUCKET,
+            s3_conn.get_object(Bucket=GROBID_XML_BUCKET,
                                Key=self.grobid_s3_key(doi))
             return True
         except botocore.exceptions.ClientError as e:
             return False
 
 
-PDFVersion.s3 = boto3.client('s3', verify=False)
+def save_pdf(doi, content, version=PDFVersion.PUBLISHED):
+    if not content:
+        return False
+    logger.info(
+        f'saving {len(content)} characters to {version.s3_url(doi)}')
+    try:
+        s3_conn.put_object(
+            Body=content,
+            Bucket=PDF_ARCHIVE_BUCKET,
+            Key=version.s3_key(doi)
+        )
+        return True
+    except Exception as e:
+        logger.error(f'failed to save pdf: {e}')
+        return False
+
+
+def enqueue_pdf_parsing(doi, version: PDFVersion = PDFVersion.PUBLISHED, commit=True):
+    db.session.execute(text(
+        "INSERT INTO recordthresher.pdf_update_ingest (doi, pdf_version) VALUES (:doi, :version) ON CONFLICT(doi) DO UPDATE SET finished = NULL;").bindparams(
+        doi=doi, version=version.value))
+    if commit:
+        db.session.commit()
