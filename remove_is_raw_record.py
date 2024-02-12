8fca3fae0bbf0eab2c928ab9a47ca3c9a333d7ea
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-02-12 15:28:34 -0600

    Logging

--- remove_is_raw_record.py
+++ remove_is_raw_record.py
@@ -2,10 +2,12 @@ from app import oa_db_engine
 from util import chunks
 
 with oa_db_engine.connect() as db_conn:
+    print(f'[*] Fetching raw record marker IDs...')
     all_records = db_conn.execute('SELECT id FROM public.tmp_rorr_marked_raw_records').fetchall()
     all_records = [record[0] for record in all_records]
     page = 1
     chunk_size = 10_000
+    print(f'[*] Removing raw record marker from {len(all_records)} records...')
     for i, chunk in enumerate(chunks(all_records, chunk_size)):
         db_conn.execute('''UPDATE ins.recordthresher_record SET authors = json_remove_array_element(authors::json, '{"is_raw_record": true}') WHERE id IN :chunk''', (tuple(chunk), ))
         page += 1
