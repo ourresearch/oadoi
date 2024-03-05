f06027705b9d0165d010b52ebe026c3e5bc782a5
Casey M <caseym@gmail.com>
2024-03-05 09:44:43 -0600

    refactor datacite record

--- recordthresher/datacite_doi_record.py
+++ recordthresher/datacite_doi_record.py
@@ -17,35 +17,58 @@ class DataCiteDoiRecord(Record):
     __mapper_args__ = {'polymorphic_identity': 'datacite_doi'}
 
     @staticmethod
-    def from_doi(doi):
+    def from_doi(cls, doi):
         if not doi:
             return None
 
-        datacite_row = DataCiteRaw.query.get(doi)
-        datacite_work = datacite_row.datacite_api_raw if datacite_row else None
+        datacite_work = cls.get_datacite_work(doi)
+
         if not datacite_work:
             return None
 
+        record = cls.get_or_create_record(doi)
+
+        record.set_doi(datacite_work)
+        record.set_title(datacite_work)
+        record.set_authors(datacite_work)
+        record.set_abstract(datacite_work)
+        record.set_published_date(datacite_work)
+        record.set_genre(datacite_work)
+        record.publisher = datacite_work['attributes'].get('publisher', None)
+        record.record_webpage_url = datacite_work['attributes'].get('url', None)
+        record.set_license(datacite_work)
+        record.set_funders(datacite_work)
+
+        if db.session.is_modified(record):
+            record.updated = datetime.datetime.utcnow().isoformat()
+
+        logger.info(f'updated record {record.id} from datacite doi {doi}')
+        return record
+
+    @classmethod
+    def get_datacite_work(cls, doi):
+        datacite_row = DataCiteRaw.query.get(doi)
+        return datacite_row.datacite_api_raw if datacite_row else None
+
+    @classmethod
+    def get_or_create_record(cls, doi):
         record_id = shortuuid.encode(
             uuid.UUID(bytes=hashlib.sha256(f'datacite_record:{doi}'.encode('utf-8')).digest()[0:16])
         )
-
-        record = DataCiteDoiRecord.query.get(record_id)
-        is_new = False
-
+        record = cls.query.get(record_id)
         if not record:
-            record = DataCiteDoiRecord(id=record_id)
-            is_new = True
+            record = cls(id=record_id)
+        return record
 
-        # doi
-        record.doi = clean_doi(datacite_work['id'])
+    def set_doi(self, datacite_work):
+        self.doi = clean_doi(datacite_work['id'])
 
-        # title
-        record.title = datacite_work['attributes']['titles'][0]['title'] if datacite_work['attributes']['titles'] else None
-        record.normalized_title = normalize_title(record.title)
+    def set_title(self, datacite_work):
+        self.title = datacite_work['attributes']['titles'][0]['title'] if datacite_work['attributes']['titles'] else None
+        self.normalized_title = normalize_title(self.title)
 
-        # authors
-        record.authors = []
+    def set_authors(self, datacite_work):
+        self.authors = []
         for datacite_author in datacite_work['attributes'].get('creators', []):
             record_author = {
                 'name': datacite_author.get('name', None),
@@ -56,54 +79,38 @@ class DataCiteDoiRecord(Record):
             for name_identifier in datacite_author.get('nameIdentifiers', []):
                 if name_identifier['nameIdentifierScheme'] == 'ORCID':
                     record_author['orcid'] = name_identifier['nameIdentifier']
-            record.authors.append(record_author)
+            self.authors.append(record_author)
+        self.authors = json.dumps(self.authors)
 
-        # abstract
+    def set_abstract(self, datacite_work):
         descriptions = datacite_work['attributes'].get('descriptions', [])
         abstract = next((d['description'] for d in descriptions if d['descriptionType'] == 'Abstract'), None)
-        record.abstract = abstract
+        self.abstract = abstract
 
-        # published date
+    def set_published_date(self, datacite_work):
         for date in datacite_work['attributes'].get('dates', []):
             if date['dateType'] == 'Issued':
                 if date['date'] and len(date['date']) == 4:
-                    record.published_date = f'{date["date"]}-01-01'
-                record.published_date = date['date']
+                    self.published_date = f'{date["date"]}-01-01'
+                self.published_date = date['date']
 
-        # genre
+    def set_genre(self, datacite_work):
         genre = datacite_work['attributes'].get('types', {}).get('bibtex', None)
         genre = genre.lower().strip() if genre else None
-        record.genre = genre
+        self.genre = genre
 
-        # publisher
-        record.publisher = datacite_work['attributes'].get('publisher', None)
-
-        # webpage url
-        record.record_webpage_url = datacite_work['attributes'].get('url', None)
-
-        # license
+    def set_license(self, datacite_work):
         for rights in datacite_work['attributes'].get('rightsList', []):
             if rights.get('rightsIdentifier', None):
-                record.open_license = rights.get('rightsIdentifier', None)
+                self.open_license = rights.get('rightsIdentifier', None)
 
-        # funders
-        record.funders = []
+    def set_funders(self, datacite_work):
+        self.funders = []
         for funder in datacite_work['attributes'].get('fundingReferences', []):
             record_funder = {
                 'name': funder.get('funderName', None),
                 'award': funder.get('awardNumber', None),
                 'doi': funder.get('funderIdentifier', None)
             }
-            record.funders.append(record_funder)
-
-        record.authors = json.dumps(record.authors)
-        record.funders = json.dumps(record.funders)
-
-        if db.session.is_modified(record):
-            record.updated = datetime.datetime.utcnow().isoformat()
-
-        if is_new:
-            logger.info(f'created record {record.id} from doi {doi}')
-        else:
-            logger.info(f'updated record {record.id} from doi {doi}')
-        return record
+            self.funders.append(record_funder)
+        self.funders = json.dumps(self.funders)
