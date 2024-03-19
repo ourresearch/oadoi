18a39adde555cc243e23c3e028829219cc85832f
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-03-19 08:34:01 -0500

    PDFRecordMaker refactor Improve logging

--- recordthresher/record_maker/pdf_record_maker.py
+++ recordthresher/record_maker/pdf_record_maker.py
@@ -19,15 +19,20 @@ def pdf_parse_api_url(pub):
 
 
 class PDFRecordMaker:
+
+    @classmethod
+    def new_id(cls, doi):
+        return shortuuid.encode(
+            uuid.UUID(bytes=hashlib.sha256(
+                f'parsed_pdf:{doi}'.encode('utf-8')).digest()[0:16])
+        )
+
     @classmethod
     def make_record(cls, pub, update_existing=True):
         if not (pub and hasattr(pub, 'id') and pub.id):
             return None
 
-        record_id = shortuuid.encode(
-            uuid.UUID(bytes=hashlib.sha256(
-                f'parsed_pdf:{pub.id}'.encode('utf-8')).digest()[0:16])
-        )
+        record_id = cls.new_id(pub.id)
 
         pdf_record = PDFRecord.query.get(record_id)
 
@@ -36,25 +41,38 @@ class PDFRecordMaker:
                 f"not updating existing pdf record {pdf_record.id}")
             return None
 
-        r_json = pdf_parser_response(pdf_parse_api_url(pub))
-        msg = r_json.get('message', {}) or {}
+        parsed_pdf_json = pdf_parser_response(pdf_parse_api_url(pub))
+        return cls.make_pdf_record(pub.id,
+                                   parsed_pdf_json,
+                                   pdf_record)
+
+    @classmethod
+    def make_pdf_record(cls, doi, parsed_pdf_json, existing_record=None):
+        if 'authors' in parsed_pdf_json:
+            msg = parsed_pdf_json
+        else:
+            msg = parsed_pdf_json.get('message', {}) or {}
 
         has_data = any([bool(msg.get(key)) for key in msg.keys()])
 
-        if not r_json or not has_data:
+        if not parsed_pdf_json:
             logger.info(
-                f"didn't get a pdf parse response for {pub.id}, not making record")
+                f"didn't get a pdf parse response for {doi}, not making record")
             return None
 
-        authors = r_json.get('authors')
-        references = r_json.get('references')
-        pdf_record = pdf_record or PDFRecord(id=record_id)
+        if not has_data:
+            logger.info(f'grobid pdf data is empty for {doi}, not making record')
+            return None
+
+        authors = parsed_pdf_json.get('authors')
+        references = parsed_pdf_json.get('references')
+        pdf_record = existing_record or PDFRecord(id=cls.new_id(doi))
         pdf_record.authors = (authors and json.dumps(authors)) or '[]'
-        pdf_record.published_date = r_json.get('published_date')
-        pdf_record.genre = r_json.get('genre')
-        pdf_record.abstract = r_json.get('abstract')
+        pdf_record.published_date = parsed_pdf_json.get('published_date')
+        pdf_record.genre = parsed_pdf_json.get('genre')
+        pdf_record.abstract = parsed_pdf_json.get('abstract')
         pdf_record.citations = (references and json.dumps(references)) or '[]'
-        pdf_record.doi = pub.id
+        pdf_record.doi = doi
         pdf_record.work_id = -1
         pdf_record.updated = datetime.datetime.utcnow().isoformat()
 
