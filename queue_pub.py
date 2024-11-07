1c716389758dbf529c313f4fd343308808dd2d09
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-11-07 09:08:46 -0600

    Allow update multiple DOIs in queue_pub.py cli

--- queue_pub.py
+++ queue_pub.py
@@ -30,14 +30,14 @@ class DbQueuePub(DbQueue):
         return process_name
 
     def worker_run(self, **kwargs):
-        single_obj_id = kwargs.get("id", None)
+        dois = kwargs.get("doi", [])
         chunk = kwargs.get("chunk", 100)
         limit = kwargs.get("limit", 10)
         run_class = Pub
         run_method = kwargs.get("method")
 
-        if single_obj_id:
-            limit = 1
+        if dois:
+            limit = len(dois)
             queue_table = None
         elif run_method == "refresh":
             queue_table = "pub_refresh_queue"
@@ -88,68 +88,67 @@ class DbQueuePub(DbQueue):
                 chunk=chunk,
                 queue_table=queue_table
             )
+
         index = 0
         start_time = time()
         oa_db_conn = oa_db_engine.connect()
         oa_redis_conn = Redis.from_url(os.environ["REDIS_DO_URL"])
+
         while True:
             new_loop_start_time = time()
-            if single_obj_id:
-                single_obj_id = normalize_doi(single_obj_id)
-                objects = [run_class.query.filter(run_class.id == single_obj_id).first()]
+            if dois:
+                normalized_dois = [normalize_doi(doi) for doi in dois]
+                objects = run_class.query.filter(
+                    run_class.id.in_(normalized_dois)).all()
+                if not objects:
+                    logger.info(f"No publications found for DOIs: {dois}")
+                    return
             else:
                 logger.info("looking for new jobs")
 
                 job_time = time()
-                row_list = db.engine.execute(text(text_query).execution_options(autocommit=True)).fetchall()
+                row_list = db.engine.execute(text(text_query).execution_options(
+                    autocommit=True)).fetchall()
                 object_ids = [row[0] for row in row_list]
-                logger.info("got ids, took {} seconds".format(elapsed(job_time)))
+                logger.info(
+                    "got ids, took {} seconds".format(elapsed(job_time)))
 
                 job_time = time()
-                q = db.session.query(Pub).options(orm.undefer('*')).filter(Pub.id.in_(object_ids))
+                q = db.session.query(Pub).options(orm.undefer('*')).filter(
+                    Pub.id.in_(object_ids))
                 objects = q.all()
-                logger.info("got pub objects in {} seconds".format(elapsed(job_time)))
+                logger.info(
+                    "got pub objects in {} seconds".format(elapsed(job_time)))
 
-                # shuffle them or they sort by doi order
                 random.shuffle(objects)
 
-                # objects = Pub.query.from_statement(text(text_query)).execution_options(autocommit=True).all()
-
-                # objects = run_class.query.from_statement(text(text_query)).execution_options(autocommit=True).all()
-                # id_rows =  db.engine.execute(text(text_query)).fetchall()
-                # ids = [row[0] for row in id_rows]
-                #
-                # job_time = time()
-                # objects = run_class.query.filter(run_class.id.in_(ids)).all()
-
-                # logger.info(u"finished get-new-objects query in {} seconds".format(elapsed(job_time)))
-
             if not objects:
-                # logger.info(u"sleeping for 5 seconds, then going again")
-                sleep(5)
-                continue
+                if not dois:  # Only sleep and continue if we're processing from queue
+                    sleep(5)
+                    continue
+                return
 
             object_ids = [obj.id for obj in objects]
             self.update_fn(run_class, run_method, objects, index=index)
             enqueue_unpaywall_refresh(object_ids, oa_db_conn, oa_redis_conn)
-            logger.info(f'Enqueued {len(object_ids)} works to be updated in unpaywall_recordthresher_fields')
+            logger.info(
+                f'Enqueued {len(object_ids)} works to be updated in unpaywall_recordthresher_fields')
 
-            # logger.info(u"finished update_fn")
             if queue_table:
-                object_ids_str = ",".join(["'{}'".format(id.replace("'", "''")) for id in object_ids])
-                object_ids_str = object_ids_str.replace("%", "%%")  #sql escaping
+                object_ids_str = ",".join(
+                    ["'{}'".format(id.replace("'", "''")) for id in object_ids])
+                object_ids_str = object_ids_str.replace("%",
+                                                        "%%")  # sql escaping
                 sql_command = "update {queue_table} set finished=now(), started=null where id in ({ids})".format(
                     queue_table=queue_table, ids=object_ids_str)
-                # logger.info(u"sql command to update finished is: {}".format(sql_command))
                 run_sql(db, sql_command)
-                # logger.info(u"finished run_sql")
 
-            # finished is set in update_fn
             index += 1
-            if single_obj_id:
+            if dois:
                 return
             else:
-                self.print_update(new_loop_start_time, chunk, limit, start_time, index)
+                self.print_update(new_loop_start_time, chunk, limit, start_time,
+                                  index)
 
 
 if __name__ == "__main__":
@@ -159,22 +158,33 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Run stuff.")
     parser.add_argument('--id', nargs="?", type=str, help="id of the one thing you want to update (case sensitive)")
-    parser.add_argument('--doi', nargs="?", type=str, help="id of the one thing you want to update (case insensitive)")
-    parser.add_argument('--method', nargs="?", type=str, default="update", help="method name to run")
+    parser.add_argument('--doi', nargs="+", type=str,
+                        help="list of DOIs to update (case sensitive)")
+    parser.add_argument('--method', nargs="?", type=str, default="update",
+                        help="method name to run")
 
-    parser.add_argument('--reset', default=False, action='store_true', help="do you want to just reset?")
-    parser.add_argument('--run', default=False, action='store_true', help="to run the queue")
-    parser.add_argument('--status', default=False, action='store_true', help="to logger.info(the status")
-    parser.add_argument('--dynos', default=None, type=int, help="scale to this many dynos")
-    parser.add_argument('--logs', default=False, action='store_true', help="logger.info(out logs")
-    parser.add_argument('--monitor', default=False, action='store_true', help="monitor till done, then turn off dynos")
-    parser.add_argument('--kick', default=False, action='store_true', help="put started but unfinished dois back to unstarted so they are retried")
-    parser.add_argument('--limit', "-l", nargs="?", type=int, help="how many jobs to do")
-    parser.add_argument('--chunk', "-ch", nargs="?", default=500, type=int, help="how many to take off db at once")
+    parser.add_argument('--reset', default=False, action='store_true',
+                        help="do you want to just reset?")
+    parser.add_argument('--run', default=False, action='store_true',
+                        help="to run the queue")
+    parser.add_argument('--status', default=False, action='store_true',
+                        help="to logger.info(the status")
+    parser.add_argument('--dynos', default=None, type=int,
+                        help="scale to this many dynos")
+    parser.add_argument('--logs', default=False, action='store_true',
+                        help="logger.info(out logs")
+    parser.add_argument('--monitor', default=False, action='store_true',
+                        help="monitor till done, then turn off dynos")
+    parser.add_argument('--kick', default=False, action='store_true',
+                        help="put started but unfinished dois back to unstarted so they are retried")
+    parser.add_argument('--limit', "-l", nargs="?", type=int,
+                        help="how many jobs to do")
+    parser.add_argument('--chunk', "-ch", nargs="?", default=500, type=int,
+                        help="how many to take off db at once")
 
     parsed_args = parser.parse_args()
 
-    job_type = "normal"  #should be an object attribute
+    job_type = "normal"  # should be an object attribute
     my_queue = DbQueuePub()
     my_queue.parsed_vars = vars(parsed_args)
     my_queue.run_right_thing(parsed_args, job_type)
