26fb925ae5208cdfe806eb1ea046990317a68950
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-11-15 08:08:39 -0600

    Strip trailing/leading '.' characters from DOI in autoresponder

--- views.py
+++ views.py
@@ -1118,7 +1118,7 @@ def freshdesk_autorefresh():
     doi_pattern = r'10\.\d{4,9}/[^\s"<>?]+'
     dois = re.findall(doi_pattern, description)
     dois += re.findall(doi_pattern, subject)
-    dois = set(dois)
+    dois = set([doi.strip('.') for doi in dois])
     if not dois:
         return Response(status=200)
     refreshed_dois = []
