3c8c5f5a7ba5d2d9f62a4695dfb1917377c14c76
Nolan McCafferty <nolanmccafferty@gmail.com>
2025-01-23 10:37:09 -0600

    Fix bad doi, link pair

--- reported_noncompliant_copies.py
+++ reported_noncompliant_copies.py
@@ -65,7 +65,7 @@ lookup_raw = {
     '10.5153/sro.3693':
         ['http://www.ejustice.just.fgov.be/mopdf/2013/04/11_1.pdf#Page41'],
     '10.29228/jamp.53876':
-        ['https://academicmed.org/Uploads/Volume7Issue1/1.%20[4520.%20JAMP_MOGA]%201-5.pdf']
+        ['https://academicmed.org/Uploads/Volume7Issue1/1. [4520. JAMP_MOGA] 1-5.pdf']
 }
 
 location_url_blacklist = {
