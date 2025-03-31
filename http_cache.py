cf7a01832d04604b4822a997e2c089b38babcc68
Casey M <caseym@gmail.com>
2025-03-31 18:07:57 -0500

    fix zyte bug
    (cherry picked from commit b9b3cc22b5d57bf3435296e597e7f92177ee64a9)

--- http_cache.py
+++ http_cache.py
@@ -211,6 +211,7 @@ def call_requests_get(url=None, headers=None, read_timeout=300, connect_timeout=
         url = redirected_url
 
     # default parameters setup
+    # default parameters setup
     default_params = {
         "url": url,
         "httpResponseBody": True,
@@ -222,7 +223,7 @@ def call_requests_get(url=None, headers=None, read_timeout=300, connect_timeout=
     if matching_policies:
         policy = matching_policies[min(attempt_n, len(matching_policies) - 1)]
 
-        if policy.profile == 'api':
+        if policy.profile == 'api' and policy.params is not None:
             logger.info(f'Using Zyte API profile for url: {url}')
             zyte_params = policy.params
         else:
@@ -231,6 +232,10 @@ def call_requests_get(url=None, headers=None, read_timeout=300, connect_timeout=
     else:
         logger.info(f'No matching policies - using default Zyte profile for url: {url}')
         zyte_params = default_params
+
+    if zyte_params is None:
+        logger.warning(f"zyte_params was None for url: {url}, using default params")
+        zyte_params = default_params.copy()
 
     # set URL in parameters
     zyte_params["url"] = url
