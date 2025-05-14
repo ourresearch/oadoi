93e9c09519d6ca4b74d7b3e7a47d05bc52fe74b0
Casey M <caseym@gmail.com>
2025-05-14 08:39:01 -0500

    sort response

--- wunpaywall.py
+++ wunpaywall.py
@@ -12,4 +12,31 @@ class WunpaywallPub(db.Model):
     json_response = db.Column(db.Text)
 
     def to_dict(self):
-        return json.loads(self.json_response)
+        response = json.loads(self.json_response)
+
+        sorted_response = {
+            "doi": response.get("doi"),
+            "doi_url": response.get("doi_url"),
+            "title": response.get("title"),
+            "genre": response.get("genre"),
+            "is_paratext": response.get("is_paratext"),
+            "published_date": response.get("published_date"),
+            "year": response.get("year"),
+            "journal_name": response.get("journal_name"),
+            "journal_issns": response.get("journal_issns"),
+            "journal_issn_l": response.get("journal_issn_l"),
+            "journal_is_oa": response.get("journal_is_oa"),
+            "journal_is_in_doaj": response.get("journal_is_in_doaj"),
+            "publisher": response.get("publisher"),
+            "is_oa": response.get("is_oa"),
+            "oa_status": response.get("oa_status"),
+            "has_repository_copy": response.get("has_repository_copy"),
+            "best_oa_location": response.get("best_oa_location"),
+            "first_oa_location": response.get("first_oa_location"),
+            "oa_locations": response.get("oa_locations"),
+            "oa_locations_embargoed": response.get("oa_locations_embargoed"),
+            "updated": response.get("updated"),
+            "data_standard": response.get("data_standard"),
+            "z_authors": response.get("z_authors")
+        }
+        return sorted_response
